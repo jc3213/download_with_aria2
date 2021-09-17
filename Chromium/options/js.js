e0c9a2634f84cadc1a52bcda6d1653a7225b7a03
jc3213 <jc3213@users.noreply.github.com>
2021-09-17 18:12:53 +0800

    fixed show password button in options

--- Chromium/options/js.js
+++ Chromium/options/js.js
@@ -31,7 +31,7 @@ document.querySelector('#aria2_btn').addEventListener('click', (event) => {
 });
 
 document.querySelector('#show_btn').addEventListener('click', (event) => {
-    document.querySelector('#token').setAttribute('type', event.target.className === 'checked' ? 'password' : 'text');
+    document.querySelector('[root="jsonrpc"][local="token"]').setAttribute('type', event.target.className === 'checked' ? 'password' : 'text');
     event.target.classList.toggle('checked');
 });
 
