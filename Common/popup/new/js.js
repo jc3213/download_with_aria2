98eb3d26c41c86821ee4fe747eb485592d8430f4
jc3213 <jc3213@users.noreply.github.com>
2021-09-19 14:29:07 +0800

    dropped json, add metalink support

--- Common/popup/new/js.js
+++ Common/popup/new/js.js
@@ -5,34 +5,40 @@ function aria2RPCAssist() {
 
 document.querySelector('#submit_btn').addEventListener('click', (event) => {
     var referer = document.querySelector('#referer').value;
-    var options = {};
-    document.querySelectorAll('[task], [aria2]').forEach(field => {
-        var name = field.getAttribute('aria2') ?? field.getAttribute('task');
-        options[name] = field.value;
-    });
-    document.querySelector('#entries').value.split('\n').forEach(url => {
-        try {
-            var session = JSON.parse(url);
-            if (Array.isArray(session)) {
-                if (typeof session[0] === 'string') {
-                    newDownloadRequest({url: session, referer}, options);
-                }
-                else {
-                    session.forEach(task => newDownloadRequest(referer ? {...task, referer} : task, options));
-                }
-            }
-            else {
-                newDownloadRequest(referer ? {...session, referer} : session, options);
-            }
-        }
-        catch(error) {
-            newDownloadRequest({url, referer}, options);
-        }
-    });
+    var options = newTaskOptions();
+    document.querySelector('#entries').split('\n').forEach(url => newDownloadRequest({url, referer}, options));
+    removeNewTaskWindow();
+});
+
+document.querySelector('#entries').addEventListener('drop', (event) => {
+    var file = event.dataTransfer.files[0];
+    if (file.name.endsWith('metalink')) {
+        var reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = () => {
+            var metalink = btoa(unescape(encodeURIComponent(reader.result)));
+            aria2RPCRequest({id: '', jsonrpc: 2, method: 'aria2.addMetalink', params: [token, metalink, newTaskOptions()]},
+            result => {
+                showNotification(file.name);
+                removeNewTaskWindow();
+            });
+        };
+    }
+});
+
+function removeNewTaskWindow() {
     parent.document.querySelector('[module="' + frameElement.id + '"]').classList.remove('checked');
     frameElement.style.display = 'none';
     setTimeout(() => frameElement.remove(), 500);
-});
+}
+
+function newTaskOptions(options = {}) {
+    document.querySelectorAll('[aria2], [task]').forEach(field => {
+        var name = field.getAttribute('aria2') ?? field.getAttribute('task');
+        options[name] = field.value;
+    });
+    return options;
+}
 
 function newDownloadRequest(request, options) {
     if (/^(https?|ftp):\/\/|^magnet:\?/i.test(request.url)) {
