5b5630f979337faa4a71d7113096c074ec3241d9
jc3213 <jc3213@users.noreply.github.com>
2021-09-21 06:24:15 +0800

    update build script

--- Auto Build.cmd
+++ Auto Build.cmd
@@ -12,12 +12,14 @@ IF NOT EXIST 7z.exe GOTO :Exit
 FOR /F "USEBACKQ SKIP=3 TOKENS=1,2 DELIMS=,: " %%I IN ("%~DP0%Code%\manifest.json") DO (
 	IF "%%~I"=="version" (
         SET Zip=%~DP0%Code%-%%~J.zip
-        GOTO :File
+        GOTO :Build
     )
 )
-:File
-%~DP07z.exe a %Zip% %~DP0Common\*
-%~DP07z.exe u %Zip% %~DP0%Code%\*
-PAUSE
+:Build
+ECHO =================================================
+%~DP07z.exe a %Zip% %~DP0Common\* %~DP0%Code%\*
+ECHO =================================================
 :Exit
+ECHO %Code% build completed
+PAUSE
 EXIT
