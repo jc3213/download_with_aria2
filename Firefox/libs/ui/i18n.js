0bc261b5f405d2b8afc14029d3c9728021e223eb
jc3213 <jc3213@users.noreply.github.com>
2021-09-20 12:40:44 +0800

    Delete i18n.js

--- Firefox/libs/ui/i18n.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('[i18n]').forEach(item => {
-    item.innerText = browser.i18n.getMessage(item.innerText);
-});
-
-document.querySelectorAll('[i18n_title]').forEach(item => {
-    item.title = browser.i18n.getMessage(item.title);
-});
