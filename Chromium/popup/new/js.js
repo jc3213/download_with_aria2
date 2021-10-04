55811ca0359d5550fb515f4813b09c68af323186
jc3213 <jc3213@users.noreply.github.com>
2021-10-04 12:04:42 +0800

    further optimized logic

--- Chromium/popup/new/js.js
+++ Chromium/popup/new/js.js
@@ -4,9 +4,8 @@ function aria2RPCClient() {
 }
 
 document.querySelector('#submit_btn').addEventListener('click', (event) => {
-    var header = ['Referer: ' + document.querySelector('#referer').value, 'User-Agent: ' + aria2RPC['useragent']];
     var entries = document.querySelector('#entries').value;
-    var options = newTaskOptions({header});
+    var options = newTaskOptions();
     var request = entries.split('\n').map(url => ({id: '', jsonrpc: 2, method: 'aria2.addUri', params: [aria2RPC.jsonrpc['token'], [url], options]}));
     aria2RPCRequest(request, result => showNotification(entries), showNotification);
     removeNewTaskWindow();
@@ -17,14 +16,7 @@ document.querySelector('#entries').addEventListener('drop', (event) => {
     if (file.name.endsWith('metalink') || file.name.endsWith('meta4') || file.name.endsWith('torrent')) {
         fileReader(file, (blob, filename) => {
             var params = [aria2RPC.jsonrpc['token'], blob.slice(blob.indexOf(',') + 1)];
-            var options = newTaskOptions();
-            if (filename.endsWith('torrent')) {
-                var method = 'aria2.addTorrent';
-            }
-            else {
-                method = 'aria2.addMetalink';
-                params = [...params, options];
-            }
+            var method = filename.endsWith('torrent') ? 'aria2.addTorrent' : params.push(newTaskOptions()) && 'aria2.addMetalink';
             aria2RPCRequest({id: '', jsonrpc: 2, method, params},
             result => showNotification(filename), showNotification);
             removeNewTaskWindow();
@@ -32,7 +24,8 @@ document.querySelector('#entries').addEventListener('drop', (event) => {
     }
 });
 
-function newTaskOptions(options = {}) {
+function newTaskOptions() {
+    var options = {'header': ['Referer: ' + document.querySelector('#referer').value, 'User-Agent: ' + aria2RPC['useragent']]};
     document.querySelectorAll('[aria2], [task]').forEach(field => {
         var name = field.getAttribute('aria2') ?? field.getAttribute('task');
         options[name] = field.value;
