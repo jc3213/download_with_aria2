9b4fb66bd0038dee313433d5767f930fa21ebd92
jc3213 <jc3213@users.noreply.github.com>
2021-11-13 15:35:51 +0800

    simplified logic

--- Chromium/libs/ui/size.js
+++ Chromium/libs/ui/size.js
@@ -1,7 +1,6 @@
 function bytesToFileSize(bytes) {
-    return bytes < 1024 ? bytes + ' B' :
-        bytes >= 1024 && bytes < 104857 ? (bytes / 10.24 | 0) / 100 + ' KB' :
-        bytes >= 1048576 && bytes < 1073741824 ? (bytes / 10485.76 | 0) / 100 + ' MB' :
-        bytes >= 1073741824 && bytes < 1099511627776 ? (bytes / 10737418.24 | 0) / 100 + ' GB' : 
-        (bytes / 10995116277.76 | 0) / 100 + ' TB';
+    return bytes < 0 ? '?? B' : bytes < 1024 ? bytes + ' B' :
+        bytes < 104857 ? (bytes / 10.24 | 0) / 100 + ' KB' :
+        bytes < 1073741824 ? (bytes / 10485.76 | 0) / 100 + ' MB' :
+        bytes < 1099511627776 ? (bytes / 10737418.24 | 0) / 100 + ' GB' : (bytes / 10995116277.76 | 0) / 100 + ' TB';
 }
