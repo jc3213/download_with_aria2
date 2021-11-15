d578d12bed779c0604e03412a2c09f0134dff700
jc3213 <jc3213@users.noreply.github.com>
2021-11-15 09:16:50 +0800

    clean up /libs/js/options.js

---
+++ options/aria2/js.js
@@ -0,0 +1,13 @@
+document.querySelector('#version').innerText = location.search.slice(1);
+
+document.querySelector('#back_btn').addEventListener('click', (event) => {
+    frameElement.remove();
+});
+
+document.addEventListener('change', (event) => {
+    aria2RPCRequest({id: '', jsonrpc: 2, method: 'aria2.changeGlobalOption', params: [aria2RPC.jsonrpc['token'], {[event.target.getAttribute('aria2')]: event.target.value}]});
+});
+
+function aria2RPCClient() {
+    printGlobalOption();
+}
