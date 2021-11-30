efcb816afec10cba3e86a2569a3228de72a26be9
jc3213 <jc3213@users.noreply.github.com>
2021-11-30 16:14:18 +0800

    urls protocol must be http, ftp, or magnet

--- Firefox/popup/new/js.js
+++ Firefox/popup/new/js.js
@@ -9,7 +9,7 @@ document.querySelector('#submit_btn').addEventListener('click', event => {
         var name = field.getAttribute('aria2') ?? field.getAttribute('task');
         options[name] = field.value;
     });
-    document.querySelector('#entries').value.split('\n').forEach(url => downloadWithAria2(url, options));
+    document.querySelector('#entries').value.split('\n').forEach(url => /^http|^ftp|^magnet/.test(url) ? downloadWithAria2(url, options) : null);
     parent.document.querySelector('[module="' + frameElement.id + '"]').classList.remove('checked');
     frameElement.style.display = 'none';
     setTimeout(() => frameElement.remove(), 500);
