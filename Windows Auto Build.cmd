1550ddd862c49600547ec113666515cd0707b5de
jc3213 <jc3213@users.noreply.github.com>
2021-11-30 15:25:34 +0800

    update windows build script

--- Windows Auto Build.cmd
+++ Windows Auto Build.cmd
@@ -1,5 +1,7 @@
 @ECHO OFF
 PUSHD %~DP0
+IF NOT EXIST 7za.exe GOTO :Exit
+IF NOT EXIST 7za.dll GOTO :Exit
 :Code
 ECHO Auto build script for extension ^<Download with Aria2^>
 ECHO 1. Chromium
@@ -8,21 +10,14 @@ SET /P Option=Build for:
 IF %Option% EQU 1 SET Code=Chromium
 IF %Option% EQU 2 SET Code=Firefox
 IF NOT DEFINED Code CLS && GOTO :Code
-IF NOT EXIST 7za.exe GOTO :Exit
-FOR /F "USEBACKQ SKIP=3 TOKENS=1,2 DELIMS=,: " %%I IN ("%~DP0%Code%\manifest.json") DO (
-	IF "%%~I"=="version" (
-        SET Version=%%~J
-        SET Zip=%~DP0%Code%-%%~J.zip
-        GOTO :Build
-    )
-)
+:Version
+FOR /F "USEBACKQ SKIP=3 TOKENS=1,2 DELIMS=,: " %%I IN ("%~DP0%Code%\manifest.json") DO (IF %%~I EQU version SET Version=%%~J)
 :Build
-%~DP07za.exe a %Zip% %~DP0Chromium\*
-IF %Option% EQU 2 %~DP07za.exe u %Zip% %~DP0Firefox\*
-GOTO :EXIT
-:Exit
+7za.exe a %Code%-%Version%.zip Chromium\*
+IF %Option% EQU 2 7za.exe u %Code%-%Version%.zip Firefox\*
 ECHO.
 ECHO.
 ECHO %Code% %Version% build completed, script will be terminated in 5 seconds...
+:Exit
 TIMEOUT /T 5
 EXIT
