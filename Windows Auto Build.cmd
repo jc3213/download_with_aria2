e2caee77850dbbdcfef3f51d2cf2bb8151a3014d
jc3213 <jc3213@users.noreply.github.com>
2021-12-15 16:34:46 +0800

    fixed a bug at false value input

--- Windows Auto Build.cmd
+++ Windows Auto Build.cmd
@@ -1,4 +1,5 @@
 @ECHO OFF
+:Code
 ECHO Auto build script for extension ^<Download with Aria2^>
 ECHO 1. Chromium
 ECHO 2. Firefox
@@ -6,14 +7,10 @@ SET /P Option=Build for:
 IF %Option% EQU 1 SET Code=Chromium
 IF %Option% EQU 2 SET Code=Firefox
 IF NOT DEFINED Code CLS && GOTO :Code
-:Version
 FOR /F "USEBACKQ SKIP=3 TOKENS=1,2 DELIMS=,: " %%I IN ("%~DP0%Code%\manifest.json") DO (IF %%~I EQU version SET Version=%%~J)
-:Build
 %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Compress-Archive -Path '%~DP0Chromium\*' -DestinationPath '%~DP0%Code%-%Version%.zip'"
 IF %Option% EQU 2 %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Compress-Archive -Path '%~DP0Firefox\*' -Update -DestinationPath '%~DP0%Code%-%Version%.zip'"
-ECHO.
-ECHO.
-ECHO %Code% %Version% build completed, script will be terminated in 5 seconds...
+ECHO Download with Aria2 for %Code% version %Version% build completed...
 :Exit
 TIMEOUT /T 5
 EXIT
