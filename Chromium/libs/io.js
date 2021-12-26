a8c45f291534fb61026e7f8af55db3347f19dc11
jc3213 <jc3213@users.noreply.github.com>
2021-12-26 14:38:14 +0800

    better readability

--- Chromium/libs/io.js
+++ Chromium/libs/io.js
@@ -1,6 +1,10 @@
-document.querySelectorAll('[i18n]').forEach(item => item.innerText = chrome.i18n.getMessage(item.innerText));
+document.querySelectorAll('[i18n]').forEach(item => {
+    item.innerText = chrome.i18n.getMessage(item.innerText);
+});
 
-document.querySelectorAll('[i18n_title]').forEach(item => item.title = chrome.i18n.getMessage(item.title));
+document.querySelectorAll('[i18n_title]').forEach(item => {
+    item.title = chrome.i18n.getMessage(item.title);
+});
 
 function readFileAsBinary(file, resolve) {
     var reader = new FileReader();
