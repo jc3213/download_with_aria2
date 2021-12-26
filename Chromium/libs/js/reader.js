a162f6979b0900c713d435413d9d2d048bea9542
jc3213 <jc3213@users.noreply.github.com>
2021-12-26 12:22:46 +0800

    Delete reader.js

--- Chromium/libs/js/reader.js
+++
@@ -1,5 +0,0 @@
-function fileReader(file, resolve) {
-    var reader = new FileReader();
-    reader.onload = () => resolve(reader.result.slice(reader.result.indexOf(',') + 1));
-    reader.readAsDataURL(file);
-}
