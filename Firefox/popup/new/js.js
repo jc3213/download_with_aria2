04e4e394fa0768314702797f10217659ee54b060
jc3213 <jc3213@users.noreply.github.com>
2022-01-04 09:38:07 +0800

    add button for firefox

--- Firefox/popup/new/js.js
+++ Firefox/popup/new/js.js
@@ -3,6 +3,11 @@ function aria2RPCClient() {
     printFeedButton();
 }
 
+document.querySelector('#add_btn').addEventListener('click', async event => {
+    var tabs = await browser.tabs.query({active: true, currentWindow: true});
+    document.querySelector('#referer').value = tabs[0].url;
+});
+
 document.querySelector('#submit_btn').addEventListener('click', event => {
     var options = {'header': ['Referer: ' + document.querySelector('#referer').value, 'User-Agent: ' + aria2RPC['useragent']]};
     document.querySelectorAll('[aria2], [task]').forEach(field => {
