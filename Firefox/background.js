816eebc28e31de3e460deb280e6c7cf93c28481a
jc3213 <jc3213@users.noreply.github.com>
2022-01-05 11:19:03 +0800

    notification error use E for popup icon

--- Firefox/background.js
+++ Firefox/background.js
@@ -100,5 +100,5 @@ function getFileExtension(filename) {
 function aria2RPCClient() {
     aria2RPCRequest({id: '', jsonrpc: 2, method: 'aria2.getGlobalStat', params: [aria2RPC.jsonrpc['token']]},
     global => browser.browserAction.setBadgeText({text: global.numActive === '0' ? '' : global.numActive}),
-    showNotification, true);
+    error => browser.browserAction.setBadgeText({text: 'E'}) ?? showNotification(error), true);
 }
