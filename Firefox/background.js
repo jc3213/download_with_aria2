182cf0fac44334c626bd788a9b989ed612ffd2eb
jc3213 <jc3213@users.noreply.github.com>
2022-01-05 14:20:36 +0800

    removed fileSize work around

--- Firefox/background.js
+++ Firefox/background.js
@@ -27,51 +27,29 @@ browser.downloads.onCreated.addListener(async item => {
     var folder = aria2RPC.folder['mode'] === '1' ? item.filename.slice(0, item.filename.indexOf(filename)) : aria2RPC.folder['mode'] === '2' ? aria2RPC.folder['uri'] : null;
     var storeId = tabs[0].cookieStoreId;
 
-    if (await captureDownload(domain, getFileExtension(filename), url)) {
+    captureDownload(domain, getFileExtension(filename), url) &&
         browser.downloads.cancel(item.id).then(() => {
             browser.downloads.erase({id: item.id}).then(() => {
                 startDownload({url, referer, domain, filename, folder, storeId});
             });
         }).catch(error => showNotification('Download is already complete'));
-    }
 });
 
 async function startDownload({url, referer, domain, filename, folder, storeId}, options = {}) {
     var cookies = await browser.cookies.getAll({url, storeId});
     options['header'] = ['Cookie:', 'Referer: ' + referer, 'User-Agent: ' + aria2RPC['useragent']];
     cookies.forEach(cookie => options['header'][0] += ' ' + cookie.name + '=' + cookie.value + ';');
-    if (folder) {
-        options['dir'] = folder;
-    }
-    if (filename) {
-        options['out'] = filename;
-    }
-    if (aria2RPC.proxy['resolve'].includes(domain)) {
-        options['all-proxy'] = aria2RPC.proxy['uri'];
-    }
+    options['out'] = filename;
+    options['all-proxy'] = aria2RPC.proxy['resolve'].includes(domain) ? aria2RPC.proxy['uri'] : '';
+    folder && (options['dir'] = folder);
     downloadWithAria2(url, options);
 }
 
-async function captureDownload(domain, fileExt, url) {
-    if (aria2RPC.capture['reject'].includes(domain)) {
-        return false;
-    }
-    if (aria2RPC.capture['mode'] === '2') {
-        return true;
-    }
-    if (aria2RPC.capture['resolve'].includes(domain)) {
-        return true;
-    }
-    if (aria2RPC.capture['fileExt'].includes(fileExt)) {
-        return true;
-    }
-// Use Fetch to resolve fileSize untill Mozilla fixes downloadItem.fileSize
-// Some websites will not support this workaround due to their access policy
-// See https://bugzilla.mozilla.org/show_bug.cgi?id=1666137 for more details
-    if (aria2RPC.capture['fileSize'] > 0 && await fetch(url, {method: 'HEAD'}).then(response => response.headers.get('content-length')) >= aria2RPC.capture['fileSize']) {
-        return true;
-    }
-    return false;
+function captureDownload(domain, fileExt, url) {
+    return aria2RPC.capture['reject'].includes(domain) ? false :
+        aria2RPC.capture['mode'] === '2' ? true :
+        aria2RPC.capture['resolve'].includes(domain) ? true :
+        aria2RPC.capture['fileExt'].includes(fileExt) ? true : false;
 }
 
 function getDomainFromUrl(url) {
