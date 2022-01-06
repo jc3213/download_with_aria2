1f2b66ea115722c3566da498187ddfb3227dfd43
jc3213 <jc3213@users.noreply.github.com>
2022-01-06 20:57:41 +0800

    update code

--- Firefox/popup/new/js.js
+++ Firefox/popup/new/js.js
@@ -1,8 +1,3 @@
-function aria2RPCClient() {
-    aria2RPCCall({method: 'aria2.getGlobalOption'}, options => document.querySelectorAll('[aria2]').forEach(aria2 => parseValueToOption(aria2, 'aria2', options)));
-    printFeedButton();
-}
-
 document.querySelector('#add_btn').addEventListener('click', async event => {
     var tabs = await browser.tabs.query({active: true, currentWindow: true});
     document.querySelector('#referer').value = tabs[0].url;
@@ -12,6 +7,11 @@ document.querySelector('#submit_btn').addEventListener('click', event => {
     var options = {'header': ['Referer: ' + document.querySelector('#referer').value, 'User-Agent: ' + aria2RPC['useragent']]};
     document.querySelectorAll('[aria2], [task]').forEach(field => options[field.getAttribute('aria2') ?? field.getAttribute('task')] = field.value);
     var entries = document.querySelector('#entries').value.match(/(https?:\/\/|ftp:\/\/|magnet:\?)[^\s\n]+/g);
-    Array.isArray(entries) && aria2RPCCall(entries.map(url => ({method: 'aria2.addUri', params: [[url], options]})), showNotification(entries.join()));
+    entries && aria2RPCCall(entries.map(url => ({method: 'aria2.addUri', params: [[url], options]})), showNotification(entries.join()));
     history.back();
 });
+
+function aria2RPCClient() {
+    aria2RPCCall({method: 'aria2.getGlobalOption'}, printOptions);
+    printButton();
+}
