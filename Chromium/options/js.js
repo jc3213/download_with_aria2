0888a2bb11594720bb527f04a4d37221a62ee310
jc3213 <jc3213@users.noreply.github.com>
2022-01-23 14:55:03 +0800

    oops

--- Chromium/options/js.js
+++ Chromium/options/js.js
@@ -28,7 +28,7 @@ document.querySelector('#export_btn').addEventListener('click', event => {
     saver.click();
 });
 
-document.querySelector('#import_btn').addEventListener('change', async event => {
+document.querySelector('#import_btn').addEventListener('change', event => {
     readFileAsBinary(event.target.files[0], data => {
         chrome.storage.local.set(JSON.parse(atob(data)));
         location.reload();
