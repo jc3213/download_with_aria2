51e470c00e7ebae25996c4d6677b459119c86690
jc3213 <jc3213@users.noreply.github.com>
2022-01-24 22:12:43 +0800

    use websocket instead of fetch

--- Chromium/libs/core.js
+++ Chromium/libs/core.js
@@ -1,4 +1,3 @@
-var aria2RPC;
 var Storage;
 var aria2Error = 0;
 var aria2Live;
@@ -21,23 +20,19 @@ function aria2RPCRefresh() {
 }
 
 function aria2RPCCall(call, resolve, reject, alive) {
-    var body = JSON.stringify( 'method' in call ? {id: '', jsonrpc: 2, method: call.method, params: [Storage['secret_token']].concat(call.params ?? [])}
+    var message = JSON.stringify( 'method' in call ? {id: '', jsonrpc: 2, method: call.method, params: [Storage['secret_token']].concat(call.params ?? [])}
         : {id: '', jsonrpc: 2, method: 'system.multicall', params: [ call.map(({method, params = []}) => ({methodName: method, params: [Storage['secret_token'], ...params]})) ]} );
-    aria2RPCRequest();
-    alive && (aria2Live = setInterval(aria2RPCRequest, Storage['refresh_interval']));
-
-    function aria2RPCRequest() {
-        fetch(Storage['jsonrpc_uri'], {method: 'POST', body}).then(response => {
-            if (response.status !== 200) {
-                throw new Error(response.statusText);
-            }
-            return response.json();
-        }).then(({result}) => {
-            typeof resolve === 'function' && resolve(result);
-        }).catch(error => {
-            aria2Error === 0 && typeof reject === 'function' && (aria2Error = reject(error) ?? 1);
-        });
-    }
+    var jsonrpc = new WebSocket(Storage['jsonrpc_uri'].replace('http', 'ws'));
+    jsonrpc.onopen = event => jsonrpc.send(message);
+    jsonrpc.onerror = event => {
+        var {error} = JSON.parse(event.data);
+        aria2Error === 0 && typeof reject === 'function' && (aria2Error = reject(error) ?? 1);
+    };
+    jsonrpc.onmessage = event => {
+        var {result} = JSON.parse(event.data);
+        typeof resolve === 'function' && resolve(result);
+    };
+    alive && (aria2Live = setInterval(() => jsonrpc.send(message), Storage['refresh_interval']));
 }
 
 function showNotification(message = '') {
