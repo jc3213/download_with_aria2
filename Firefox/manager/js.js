0c1ccf1c6f43193a9e5d87c6a5fee1e5451a28e0
jc3213 <jc3213@users.noreply.github.com>
2022-01-27 23:22:28 +0800

    fixed default value of legacy mode

--- Firefox/manager/js.js
+++ Firefox/manager/js.js
@@ -8,9 +8,10 @@ document.querySelector('iframe').addEventListener('load', async event => {
     var legacy = document.createElement('div');
     legacy.className = 'submenu';
     legacy.style.display = 'none';
-    legacy.innerHTML = '<div><span class="title">Capture API</span><select name="capture_api"><option value="0">downloads API</option><option value="1">webRequest API</option></select></div>\
-        <div><div><span class="title">Capture Save Folder</span><select name="folder_mode"><option value="0">Default</option><option value="1">Browser</option><option value="2">Folder</option></select><div><input name="folder_path" type="folder"></div></div></div>';
+    legacy.innerHTML = '<div><span class="title">Capture API</span> <select name="capture_api" value="1"><option value="0">downloads API</option><option value="1">webRequest API</option></select></div>\
+        <div><div><span class="title">Capture Save Folder</span> <select name="folder_mode" value="0"><option value="0">Default</option><option value="1">Browser</option><option value="2">Custom Folder</option></select><div><input name="folder_path"></div></div></div>';
     legacy.addEventListener('change', event => (store[event.target.name] = event.target.value) && browser.storage.local.set(store));
+    legacy.querySelectorAll('[name]').forEach(field => field.value = store[field.name] ?? field.value);
 
     var manager = event.target.contentDocument.querySelector('#manager');
     manager.appendChild(safemode);
