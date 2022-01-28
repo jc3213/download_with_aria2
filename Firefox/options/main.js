ea9c6c0313958c78482f32b3b9cfc0f2f937adba
jc3213 <jc3213@users.noreply.github.com>
2022-01-28 12:03:10 +0800

    update ui dependency and i18n logic

--- Firefox/options/main.js
+++ Firefox/options/main.js
@@ -5,8 +5,8 @@ document.querySelector('iframe').addEventListener('load', async event => {
         'en': {safe: 'Safe Mode', danger: 'Dangerous!', api: 'Capture API', folder: 'Capture Download Folder', '0': 'Default', '1': 'Browser', '2': 'Custom Folder'},
         'zh': {safe: '安全模式', danger: '危险！', api: '选择API', folder: '抓取下载文件夹', '0': '默认', '1': '浏览器', '2': '自定义'}
     };
-    var accept = await browser.i18n.getAcceptLanguages().then(languages => languages.filter(lang => i18n[lang]));
-    i18n = i18n[accept[0]] ?? i18n['en'];
+    var lang = browser.i18n.getUILanguage();
+    i18n = i18n[lang] ?? i18n[lang.slice(0, lang.indexOf('-'))] ?? i18n['en'];
 
     var iframe = event.target.contentDocument;
     var filters = iframe.querySelector('#option > div:nth-child(3)');
@@ -30,10 +30,12 @@ document.querySelector('iframe').addEventListener('load', async event => {
 
     var api = document.createElement('div');
     api.innerHTML = '<span class="title">' + i18n['api'] + '</span> <select name="capture_api" value="1"><option value="0">downloads API</option><option value="1">webRequest API</option></select>';
+    api.addEventListener('change', event => folder.style.display = event.target.value === '1' ? 'none' : 'block');
 
     var folder = document.createElement('div');
     folder.innerHTML = '<span class="title">' + i18n['folder'] + '</span> <select name="folder_mode" value="0"><option value="0">' + i18n['0'] + '</option><option value="1">' + i18n['1'] + '</option><option value="2">' + i18n['2'] + '</option></select>';
-    folder.addEventListener('change', event => custom.style.display = event.target.value === '2' ? 'block': 'none');
+    folder.style.display = store['capture_api'] === '1' ? 'none' : 'block';
+    folder.addEventListener('change', event => custom.style.display = event.target.value === '2' ? 'block' : 'none');
 
     var custom = document.createElement('div');
     custom.style.display = store['folder_mode'] === '2' ? 'block': 'none';
