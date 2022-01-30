jc3213/download_with_aria2

commit d93abb0e84388aec0a5a8edb9cc08deb8e7e75fe

prepare for notification options

Chromium/libs/aria2.js
@@ -25,7 +25,7 @@ function aria2WebSocket(message, resolve, reject) {
     };
 }
 
-function aria2RPCStatus(indicate, onstart, onfinish) {
+function aria2RPCStatus(indicate) {
     return new Promise(resolve => {
         aria2RPCCall({method: 'aria2.tellActive'}, result => {
             var active = result.map(({gid}) => gid);
@@ -34,10 +34,17 @@ function aria2RPCStatus(indicate, onstart, onfinish) {
             socket.onmessage = event => {
                 var {method, params: [{gid}]} = JSON.parse(event.data);
                 var index = active.indexOf(gid);
-                method === 'aria2.onDownloadStart' ? index === -1 && active.push(gid) && onstart(gid):
-                    method !=='aria2.onBtDownloadComplete' && index !== -1 && active.splice(index, 1) && onfinish(gid);
-                indicate(active.length + '') ?? resolve(socket);
+                method === 'aria2.onDownloadStart' ? index === -1 && active.push(gid) && aria2Start(gid) :
+                    method !=='aria2.onBtDownloadComplete' && index !== -1 && active.splice(index, 1) && method === 'aria2.onDownloadComplete' && aria2Complete(gid);
             };
-        }, error => indicate());
+        }, error => indicate('E'));
     });
+}
+
+function aria2Start(gid) {
+    console.log('Download start!', gid);
+}
+
+function aria2Complete(gid) {
+    console.log('Download complete!', gid);
 }

Chromium/background.js
@@ -34,12 +34,8 @@ chrome.downloads.onDeterminingFilename.addListener(({id, finalUrl, referrer, fil
 
 async function statusIndicator() {
     jsonrpc = await aria2RPCStatus(text => {
-        chrome.browserAction.setBadgeText({text: text ? text === '0' ? '' : text : 'E'});
+        chrome.browserAction.setBadgeText({text: text === '0' ? '' : text});
         chrome.browserAction.setBadgeBackgroundColor({color: text ? '#3cc' : '#c33'});
-    }, gid => {
-        console.log('Download started!', gid)
-    }, gid => {
-        console.log('Download finished!', gid);
     });
 }
 

Firefox/background.js
@@ -61,10 +61,6 @@ async function statusIndicator() {
     jsonrpc = await aria2RPCStatus(text => {
         browser.browserAction.setBadgeText({text: text ? text === '0' ? '' : text : 'E'});
         browser.browserAction.setBadgeBackgroundColor({color: text ? '#3cc' : '#c33'});
-    }, gid => {
-        console.log('Download started!', gid)
-    }, gid => {
-        console.log('Download finished!', gid);
     });
 }
 

commit 89f5b35455e62edb9105648be39acfed28b2ec55

fixed broken indicator in d93abb0

Chromium/libs/aria2.js
@@ -1,10 +1,8 @@
-var aria2Alive = -1;
-
 function aria2RPCCall(json, resolve, reject, alive) {
     var message = JSON.stringify( json.method ? {id: '', jsonrpc: 2, method: json.method, params: [aria2Store['secret_token']].concat(json.params ?? [])} :
         {id: '', jsonrpc: 2, method: 'system.multicall', params: [json.map( ({method, params = []}) => ({id: '', jsonrpc: 2, methodName: method, params: [aria2Store['secret_token'], ...params]}) )]} );
     var worker = aria2Store['jsonrpc_uri'].startsWith('http') ? aria2XMLRequest : aria2WebSocket;
-    alive && (aria2Alive = setInterval(() => worker(message, resolve, reject), aria2Store['refresh_interval']));
+    alive && setInterval(() => worker(message, resolve, reject), aria2Store['refresh_interval']);
     worker(message, resolve, reject);
 }
 
@@ -36,6 +34,7 @@ function aria2RPCStatus(indicate) {
                 var index = active.indexOf(gid);
                 method === 'aria2.onDownloadStart' ? index === -1 && active.push(gid) && aria2Start(gid) :
                     method !=='aria2.onBtDownloadComplete' && index !== -1 && active.splice(index, 1) && method === 'aria2.onDownloadComplete' && aria2Complete(gid);
+                indicate(active.length + '') ?? resolve(socket);
             };
         }, error => indicate('E'));
     });