6ea627bfab78a48878aa2a2513abcebc688418ff
jc3213 <jc3213@users.noreply.github.com>
2022-02-03 09:54:41 +0800

    oops. forget

--- Chromium/libs/tools.js
+++ Chromium/libs/tools.js
@@ -19,8 +19,8 @@ function getFileExtension(filename) {
     return filename.slice(filename.lastIndexOf('.') + 1).toLowerCase();
 }
 
-function getFileSize(bytes = -1) {
-    return bytes < 0 ? '?? B' : bytes < 1024 ? bytes + ' B' :
+function getFileSize(bytes) {
+    return isNaN(bytes) ? '??' : bytes < 1024 ? bytes + ' B' :
         bytes < 1048576 ? (bytes / 10.24 | 0) / 100 + ' KB' :
         bytes < 1073741824 ? (bytes / 10485.76 | 0) / 100 + ' MB' :
         bytes < 1099511627776 ? (bytes / 10737418.24 | 0) / 100 + ' GB' : (bytes / 10995116277.76 | 0) / 100 + ' TB';
