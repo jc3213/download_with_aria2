6323eb7fb6a771e9afc42756e9c5bbb9f92c3294
jc3213 <jc3213@users.noreply.github.com>
2022-03-08 19:00:23 +0800

    fixed badge color when error occured

--- firefox/background.js
+++ firefox/background.js
@@ -59,9 +59,9 @@ browser.webRequest.onHeadersReceived.addListener(async ({statusCode, tabId, url,
 
 function aria2StartUp() {
     aria2RPC = new Aria2(aria2Store['jsonrpc_uri'], aria2Store['secret_token']);
-    aria2RPC.indicator(text => {
-        browser.browserAction.setBadgeText({text: text === '0' ? '' : text});
-        browser.browserAction.setBadgeBackgroundColor({color: text ? '#3cc' : '#c33'});
+    aria2RPC.indicator(number => {
+        browser.browserAction.setBadgeText({text: number === 0 ? '' : number + ''});
+        browser.browserAction.setBadgeBackgroundColor({color: number === 0 ? '#3cc' : '#c33'});
     });
 }
 
