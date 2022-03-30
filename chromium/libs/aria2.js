4c259ba68c0bfa4f5fe15088aa88e1e380d65d09
jc3213 <jc3213@users.noreply.github.com>
2022-03-30 13:45:23 +0800

    implement all aria2 methods and notifications

--- chromium/libs/aria2.js
+++ chromium/libs/aria2.js
@@ -1,19 +1,11 @@
 class Aria2 {
     constructor (jsonrpc, secret) {
-        this.jsonrpc = jsonrpc;
-        this.secret = 'token:' + secret;
-        this.sender = jsonrpc.startsWith('http') ? this.http : this.socket;
-    }
-    http (body) {
-        return new Promise((resolve, reject) => {
+        let sender = jsonrpc.startsWith('http') ? body => new Promise((resolve, reject) => {
             fetch(this.jsonrpc, {method: 'POST', body})
             .then(response => response.json())
             .then(({result, error}) => result ? resolve(result) : reject())
             .catch(reject);
-        });
-    }
-    socket (message) {
-        return new Promise((resolve, reject) => {
+        }): message => new Promise((resolve, reject) => {
             var socket = new WebSocket(this.jsonrpc);
             socket.onopen = event => socket.send(message);
             socket.onclose = reject;
@@ -23,11 +15,15 @@ class Aria2 {
                 socket.close();
             };
         });
+        this.jsonrpc = jsonrpc;
+        this.secret = 'token:' + secret;
+        this.message = (method, params) => {
+            var message = JSON.stringify({id: '', jsonrpc: 2, method, params: [this.secret].concat(params ?? [])});
+            return sender(message);
+        }
     }
-    message (method, params = []) {
-        var message = JSON.stringify({id: '', jsonrpc: 2, method, params: [this.secret, ...params]});
-        return this.sender(message);
-    }
+    methods = ['aria2.addUri', 'aria2.addTorrent', 'aria2.addMetalink', 'aria2.remove', 'aria2.forceRemove', 'aria2.pause', 'aria2.pauseAll', 'aria2.forcePause', 'aria2.forcePauseAll', 'aria2.unpause', 'aria2.unpauseAll', 'aria2.tellStatus', 'aria2.getUris', 'aria2.getFiles', 'aria2.getPeers', 'aria2.getServers', 'aria2.tellActive', 'aria2.tellWaiting', 'aria2.tellStopped', 'aria2.changePosition', 'aria2.changeUri', 'aria2.getOption', 'aria2.changeOption', 'aria2.getGlobalOption', 'aria2.changeGlobalOption', 'aria2.getGlobalStat', 'aria2.purgeDownloadResult', 'aria2.removeDownloadResult', 'aria2.getVersion', 'aria2.getSessionInfo', 'aria2.shutdown', 'aria2.forceShutdown', 'aria2.saveSession', 'system.multicall', 'system.listMethods', 'system.listNotifications']
+    notifications = ['aria2.onDownloadStart', 'aria2.onDownloadPause', 'aria2.onDownloadStop', 'aria2.onDownloadComplete', 'aria2.onDownloadError', 'aria2.onBtDownloadComplete']
     indicator (onchange) {
         this.message('aria2.tellActive').then(result => {
             var active = result.map(({gid}) => gid);
