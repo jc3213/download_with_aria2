03f7f77dd63e89fe723174573a7ccf270da839c8
jc3213 <jc3213@users.noreply.github.com>
2022-04-03 21:05:52 +0800

    oops

--- chromium/libs/aria2.js
+++ chromium/libs/aria2.js
@@ -1,9 +1,7 @@
 class Aria2 {
     constructor (jsonrpc, secret) {
-        this.secret = secret ?? '';
-        if (jsonrpc) {
-            this.jsonrpc = jsonrpc;
-        }
+        this.jsonrpc = jsonrpc;
+        this.secret = secret;
     }
     get methods () {
         return ['aria2.addUri', 'aria2.addTorrent', 'aria2.addMetalink', 'aria2.remove', 'aria2.forceRemove', 'aria2.pause', 'aria2.pauseAll', 'aria2.forcePause', 'aria2.forcePauseAll', 'aria2.unpause', 'aria2.unpauseAll', 'aria2.tellStatus', 'aria2.getUris', 'aria2.getFiles', 'aria2.getPeers', 'aria2.getServers', 'aria2.tellActive', 'aria2.tellWaiting', 'aria2.tellStopped', 'aria2.changePosition', 'aria2.changeUri', 'aria2.getOption', 'aria2.changeOption', 'aria2.getGlobalOption', 'aria2.changeGlobalOption', 'aria2.getGlobalStat', 'aria2.purgeDownloadResult', 'aria2.removeDownloadResult', 'aria2.getVersion', 'aria2.getSessionInfo', 'aria2.shutdown', 'aria2.forceShutdown', 'aria2.saveSession', 'system.multicall', 'system.listMethods', 'system.listNotifications'];
