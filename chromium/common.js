bb14acc70a1408c177e09a3bee95c45bb797c80f
jc3213 <jc3213@users.noreply.github.com>
2022-04-05 10:15:41 +0800

    oops

--- chromium/common.js
+++ chromium/common.js
@@ -26,10 +26,6 @@ function aria2StartUp() {
     });
 }
 
-function aria2Terminate() {
-    self.aria2Socket && aria2Socket.readyState === 1 && aria2Socket.close();
-}
-
 function getDomainFromUrl(url) {
     try {
         var {hostname} = new URL(url);
