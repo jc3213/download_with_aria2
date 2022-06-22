1bf8b4b41d6180d20224f271ae7cebfe60526d16
jc3213 <jc3213@users.noreply.github.com>
2022-06-22 09:24:57 +0800

    better logic

--- chromium/common.js
+++ chromium/common.js
@@ -2,23 +2,22 @@ function aria2StartUp() {
     aria2RPC = new Aria2(aria2Store['jsonrpc_uri'], aria2Store['secret_token']);
     aria2RPC.message('aria2.tellActive').then(result => {
         var active = result.map(({gid}) => gid);
-        var number = active.length;
-        chrome.browserAction.setBadgeText({text: number === 0 ? '' : number + ''});
+        chrome.browserAction.setBadgeText({text: active.length === 0 ? '' : active.length + ''});
         chrome.browserAction.setBadgeBackgroundColor({color: '#3cc'});
         aria2Socket = new WebSocket(aria2Store['jsonrpc_uri'].replace('http', 'ws'));
         aria2Socket.onmessage = event => {
             var {method, params: [{gid}]} = JSON.parse(event.data);
-            if (method === 'aria2.onDownloadStart') {
-                if (active.indexOf(gid) === -1) {
-                    active.push(gid);
-                    number ++;
+            if (method !== 'aria2.onBtDownloadComplete') {
+                if (method === 'aria2.onDownloadStart') {
+                    if (active.indexOf(gid) === -1) {
+                        active.push(gid);
+                    }
                 }
+                else {
+                    active.splice(active.indexOf(gid), 1);
+                }
+                chrome.browserAction.setBadgeText({text: active.length === 0 ? '' : active.length + ''});
             }
-            else if (method !== 'aria2.onBtDownloadComplete'){
-                active.splice(active.indexOf(gid), 1);
-                number --;
-            }
-            chrome.browserAction.setBadgeText({text: number === 0 ? '' : number + ''});
         };
     }).catch(error => {
         chrome.browserAction.setBadgeText({text: 'E'});
