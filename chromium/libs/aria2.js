20426002ef32b051733c9313f7908d4e27f81ee4
jc3213 <jc3213@users.noreply.github.com>
2022-07-11 13:12:56 +0800

    improved readability

--- chromium/libs/aria2.js
+++ chromium/libs/aria2.js
@@ -1,7 +1,53 @@
 class Aria2 {
     constructor (jsonrpc, secret) {
-        this.jsonrpc = jsonrpc;
-        this.secret = secret;
+        if (jsonrpc.startsWith('http')) {
+            var sender = function (body) {
+                return new Promise(function (resolve, reject) {
+                    fetch(jsonrpc, {method: 'POST', body}).then(function (response) {
+                        return response.json();
+                    }).then(function ({result, error}) {
+                        if (result) {
+                            resolve(result);
+                        }
+                        else {
+                            reject(error);
+                        }
+                    }).catch(reject);
+                });
+            };
+        }
+        else {
+            sender = function (message) {
+                return new Promise(function (resolve, reject) {
+                    var socket = new WebSocket(jsonrpc);
+                    socket.onopen = function (event) {
+                        socket.send(message);
+                    };
+                    socket.onclose = reject;
+                    socket.onmessage = function (event) {
+                        var {result, error} = JSON.parse(event.data);
+                        if (result) {
+                            resolve(result);
+                        }
+                        else {
+                            reject(error);
+                        }
+                        socket.close();
+                    };
+                });
+            };
+        }
+        var json = {id: '', jsonrpc: '2.0', params: []};
+        if (secret) {
+            json.params.push('token:' + secret);
+        }
+        this.message = function (method, params) {
+            var request = {...json, method};
+            if (params) {
+                request.params = [...request.params, ...params];
+            }
+            return sender(JSON.stringify(request));
+        }
     }
     get methods () {
         return ['aria2.addUri', 'aria2.addTorrent', 'aria2.addMetalink', 'aria2.remove', 'aria2.forceRemove', 'aria2.pause', 'aria2.pauseAll', 'aria2.forcePause', 'aria2.forcePauseAll', 'aria2.unpause', 'aria2.unpauseAll', 'aria2.tellStatus', 'aria2.getUris', 'aria2.getFiles', 'aria2.getPeers', 'aria2.getServers', 'aria2.tellActive', 'aria2.tellWaiting', 'aria2.tellStopped', 'aria2.changePosition', 'aria2.changeUri', 'aria2.getOption', 'aria2.changeOption', 'aria2.getGlobalOption', 'aria2.changeGlobalOption', 'aria2.getGlobalStat', 'aria2.purgeDownloadResult', 'aria2.removeDownloadResult', 'aria2.getVersion', 'aria2.getSessionInfo', 'aria2.shutdown', 'aria2.forceShutdown', 'aria2.saveSession', 'system.multicall', 'system.listMethods', 'system.listNotifications'];
@@ -9,32 +55,5 @@ class Aria2 {
     get notifications () {
         return ['aria2.onDownloadStart', 'aria2.onDownloadPause', 'aria2.onDownloadStop', 'aria2.onDownloadComplete', 'aria2.onDownloadError', 'aria2.onBtDownloadComplete'];
     }
-    set jsonrpc (jsonrpc) {
-        let sender = jsonrpc.startsWith('http') ? body => new Promise((resolve, reject) => {
-            fetch(jsonrpc, {method: 'POST', body})
-            .then(response => response.json())
-            .then(({result, error}) => result ? resolve(result) : reject(error))
-            .catch(reject);
-        }) : message => new Promise((resolve, reject) => {
-            var socket = new WebSocket(jsonrpc);
-            socket.onopen = event => socket.send(message);
-            socket.onclose = reject;
-            socket.onmessage = event => {
-                var {result, error} = JSON.parse(event.data);
-                result ? resolve(result) : reject(error);
-                socket.close();
-            };
-        });
-        this.message = (method, params) => {
-            var message = {...this.json, method};
-            message.params = message.params.concat(params ?? []);
-            return sender(JSON.stringify(message));
-        }
-    }
-    set secret (secret) {
-        this.json = {id: '', jsonrpc: '2.0', params: []}
-        if (secret) {
-            this.json.params.push('token:' + secret);
-        }
-    }
+
 }
