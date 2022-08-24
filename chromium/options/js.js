b2ba26f7d8c567136097351ab8d7bd1fabcf6d92
jc3213 <jc3213@users.noreply.github.com>
2022-08-24 16:39:13 +0800

    better ui reactivity

--- chromium/options/js.js
+++ chromium/options/js.js
@@ -2,32 +2,35 @@ var mapping = ['proxy_include', 'capture_resolve', 'capture_reject', 'capture_in
 var offset = {'capture_size': 1048576};
 var savebtn = document.querySelector('#save_btn');
 var changes = [];
-var glosave = document.querySelector('#glosav_btn');
 var glochanges = [];
 location.search === '?popup' ? document.querySelector('#manager').style.display =  'none' : document.querySelector('#popup_btn').style.display = 'none';
 
 document.querySelector('#back_btn').addEventListener('click', event => {
+    printOptions(aria2Store);
+    savebtn.style.display = 'none';
     document.body.setAttribute('data-prefs', 'option');
 });
 
 document.querySelector('#aria2_btn').addEventListener('click', event => {
     aria2RPC.message('aria2.getGlobalOption').then(options => {
         aria2Global = options;
+        savebtn.style.display = 'none';
         printGlobalOptions(options, '#global [name]');
         document.body.setAttribute('data-prefs', 'global');
     });
 });
 
 savebtn.addEventListener('click', event => {
-    applyChanges(changes, aria2Store);
+    var prefs = document.body.getAttribute('data-prefs');
+    if (prefs === 'option') {
+        applyChanges(changes, aria2Store);
+        chrome.storage.local.set(aria2Store);
+    }
+    else if (prefs === 'global') {
+        applyChanges(glochanges, aria2Global);
+        aria2RPC.message('aria2.changeGlobalOption', [aria2Global]);
+    }
     savebtn.style.display = 'none';
-    chrome.storage.local.set(aria2Store);
-});
-
-glosave.addEventListener('click', event => {
-    applyChanges(glochanges, aria2Global);
-    glosave.style.display = 'none';
-    aria2RPC.message('aria2.changeGlobalOption', [aria2Global]);
 });
 
 document.querySelector('#popup_btn').addEventListener('click', event => {
@@ -70,7 +73,7 @@ document.querySelector('#global').addEventListener('change', event => {
     var old_value = aria2Global[name];
     var new_value = event.target.value;
     printChanges(name, old_value, new_value, glochanges);
-    glosave.style.display = 'inline-block';
+    savebtn.style.display = 'inline-block';
 });
 
 chrome.storage.onChanged.addListener(changes => {
