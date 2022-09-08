e48002d017d3b34353d00541ef19a314f38f2f7a
jc3213 <jc3213@users.noreply.github.com>
2022-09-08 11:28:14 +0800

    minor update

--- chromium/background.js
+++ chromium/background.js
@@ -61,8 +61,8 @@ function aria2Capture() {
 
 function getCurrentTabUrl() {
     return new Promise(resolve => {
-        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-            resolve(tabs[0].url);
+        chrome.tabs.query({active: true, currentWindow: true}, ([tab]) => {
+            resolve(tab.url);
         });
     });
 }
