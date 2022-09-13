c6403005f4045d8456bb59768ab02ffe62c6c01d
jc3213 <jc3213@users.noreply.github.com>
2022-09-13 08:04:58 +0800

    removed options chain for firefox

--- firefox/options/hotfix.js
+++ firefox/options/hotfix.js
@@ -38,57 +38,21 @@ var api = document.createElement('div');
 api.title = i18n['api_title'];
 api.style.display = 'none';
 api.innerHTML = '<span class="title">' + i18n['api'] + '</span><select name="capture_api"><option value="0">downloads API</option><option value="1">webRequest API</option></select>';
-api.addEventListener('change', event => {
-    if (event.target.value === '1') {
-        setDefaultFolder();
-    }
-    else {
-        sub.style.display = 'block';
-    }
-});
 
 var sub = document.createElement('option');
 sub.value = '2';
 sub.innerText = i18n['browser'];
 
 var capture = document.querySelector('#option > div:nth-child(12)');
-capture.addEventListener('change', event => {
-    if ('1,2'.includes(event.target.value)) {
-        if (danger) {
-            api.style.display = 'block';
-            safe.style.display = 'none';
-        }
-        else {
-            safe.style.display = 'inline-block';
-            api.style.display = 'none';
-        }
-        sub.style.display = 'block';
-    }
-    else {
-        safe.style.display = api.style.display = 'none';
-        setDefaultFolder();
-    }
-});
-    
 var folder = document.querySelector('#option > div:nth-child(5) > select');
-folder.append(sub);
 
+folder.append(sub);
 capture.append(safe);
 capture.after(api);
-    
-function setDefaultFolder() {
-    if (folder.value === '2') {
-        folder.value = '0';
-        changes.push({name: 'folder_mode', old_value: '2', new_value: '0'});
-    }
-    sub.style.display = 'none';
-}
 
 var observer = setInterval(() => {
     if (aria2Store) {
         clearInterval(observer);
-        safe.style.display = '1,2'.includes(aria2Store['capture_mode']) ? 'inline-block' : 'none';
-        sub.style.display = '1,2'.includes(aria2Store['capture_mode']) && aria2Store['capture_api'] === '0' ? 'block' : 'none';
         folder.value = aria2Store['folder_mode'];
         api.querySelector('select').value = aria2Store['capture_api'];
     }
