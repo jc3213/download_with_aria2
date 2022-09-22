ddb19e07a3a562c68e3329765fd7793ffd80f722
jc3213 <jc3213@users.noreply.github.com>
2022-09-22 09:00:03 +0800

    changed mv3 indicator char

--- chromium_mv3/indicator.js
+++ chromium_mv3/indicator.js
@@ -1,6 +1,6 @@
 function aria2Status() {
     aria2RPC.message('aria2.getGlobalOption').then(result => {
-        chrome.action.setBadgeText({text: 'A'});
+        chrome.action.setBadgeText({text: 'R'});
         chrome.action.setBadgeBackgroundColor({color: '#33c'});
     }).catch(error => {
         chrome.action.setBadgeText({text: 'E'});
