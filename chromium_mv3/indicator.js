d29e289504ce894ecd56b8973c8994bde67b2208
jc3213 <jc3213@users.noreply.github.com>
2022-11-10 09:33:47 +0800

    only notify error

--- chromium_mv3/indicator.js
+++ chromium_mv3/indicator.js
@@ -1,8 +1,5 @@
 function aria2Status() {
-    aria2RPC.message('aria2.getGlobalOption').then(result => {
-        chrome.action.setBadgeText({text: 'R'});
-        chrome.action.setBadgeBackgroundColor({color: '#33c'});
-    }).catch(error => {
+    aria2RPC.message('aria2.getGlobalOption').catch(error => {
         chrome.action.setBadgeText({text: 'E'});
         chrome.action.setBadgeBackgroundColor({color: '#c33'});
     });
