d839f7d67c8b72251d668a35ae535ec1e34b67d5
jc3213 <jc3213@users.noreply.github.com>
2023-01-22 14:49:23 +0800

    Delete indicator.js

--- chromium_mv3/indicator.js
+++
@@ -1,9 +0,0 @@
-function aria2Update() {
-    aria2RPC = new Aria2(aria2Store['jsonrpc_uri'], aria2Store['secret_token']);
-    aria2RPC.call('aria2.getGlobalOption').then(result => {
-        chrome.action.setBadgeText({text: ''});
-    }).catch(error => {
-        chrome.action.setBadgeText({text: 'E'});
-        chrome.action.setBadgeBackgroundColor({color: '#c33'});
-    });
-}
