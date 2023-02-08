ee573144aa9e42619e6ba5efbec9ad54785ca893
jc3213 <jc3213@users.noreply.github.com>
2023-02-08 12:09:31 +0800

    Delete message.js

--- chromium/content/message.js
+++
@@ -1,8 +0,0 @@
-postMessage({extension_name: 'Download With Aria2'});
-
-addEventListener('message', event => {
-    var {aria2c, type, message} = event.data;
-    if (aria2c === 'Download With Aria2' && type === 'download') {
-        chrome.runtime.sendMessage({type, message});
-    }
-});
