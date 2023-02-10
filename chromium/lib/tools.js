56a8c78c7abc008f18477f9f4fb2b4811d7788ba
jc3213 <jc3213@users.noreply.github.com>
2023-02-10 10:22:42 +0800

    better open new window logic

--- chromium/lib/tools.js
+++ chromium/lib/tools.js
@@ -29,15 +29,15 @@ function getCurrentWindow() {
     });
 }
 
-function getNewPopup(url, offsetHeight) {
+function getNewWindow(url, offsetWidth, offsetHeight) {
     return new Promise(async resolve => {
         var {width, height, left, top} = await getCurrentWindow();
         top += (height - offsetHeight) / 2 | 0;
-        left += (width - 740) / 2 | 0;
+        left += (width - offsetWidth) / 2 | 0;
         chrome.windows.create({
             type: 'popup',
             url,
-            width: 680,
+            width: offsetWidth,
             height: offsetHeight,
             left,
             top
@@ -58,5 +58,5 @@ function aria2NewDownload(slim) {
         url = '/page/newdl.html';
         height = 726;
     }
-    return getNewPopup(url, height);
+    return getNewWindow(url, 640, height);
 }
