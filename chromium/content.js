fba8afbdd43e69fc9998f3b53218ba4229c2d600
jc3213 <jc3213@users.noreply.github.com>
2023-02-10 15:43:41 +0800

    typo

--- chromium/content.js
+++ chromium/content.js
@@ -3,6 +3,6 @@ postMessage({extension_name: 'Download With Aria2'});
 addEventListener('message', event => {
     var {aria2c, download} = event.data;
     if (aria2c === 'Download With Aria2' && download !== undefined) {
-        chrome.runtime.sendMessage({action: 'external_download', params: message});
+        chrome.runtime.sendMessage({action: 'external_download', params: download});
     }
 });
