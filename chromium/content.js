833076ea12000d0a4004de7b74428d4cf7b00f8e
jc3213 <jc3213@users.noreply.github.com>
2023-02-25 19:06:34 +0800

    better readability

--- chromium/content.js
+++ chromium/content.js
@@ -4,9 +4,9 @@ chrome.runtime.onMessage.addListener(message => {
     }
 });
 
-postMessage({extension_name: 'Download With Aria2'});
+window.postMessage({extension_name: 'Download With Aria2'});
 
-addEventListener('message', event => {
+window.addEventListener('message', event => {
     var {aria2c, download} = event.data;
     if (aria2c === 'Download With Aria2' && download !== undefined) {
         chrome.runtime.sendMessage({action: 'external_download', params: download});
