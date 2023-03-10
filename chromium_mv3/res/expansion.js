b01cb3e4e67af57ff46989f17adafe874be7a930
jc3213 <jc3213@users.noreply.github.com>
2023-03-10 09:24:08 +0800

    v4.2.1.1911

--- chromium_mv3/res/expansion.js
+++ chromium_mv3/res/expansion.js
@@ -41,12 +41,3 @@ function aria2Manager() {
 function aria2Client() {
     aria2RPC = new Aria2(aria2Store['jsonrpc_uri'], aria2Store['jsonrpc_token']);
 }
-
-function aria2Badge() {
-    aria2RPC.call('aria2.getGlobalOption').then(result => {
-        chrome.action.setBadgeText({text: ''});
-    }).catch(error => {
-        chrome.action.setBadgeText({text: 'E'});
-        chrome.action.setBadgeBackgroundColor({color: '#c33'});
-    });
-}
