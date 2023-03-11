7113d4f8c2aa39e48afeaaa68e8763060adcd2f0
jc3213 <jc3213@users.noreply.github.com>
2023-03-11 08:15:59 +0800

    v4.2.1.1912

--- chromium/res/expansion.js
+++ chromium/res/expansion.js
@@ -1,5 +1,7 @@
 var aria2Popup = 'page/popup.html';
 var aria2Socket;
+var aria2Retry;
+var aria2Active;
 
 chrome.browserAction.onClicked.addListener(tab => {
     chrome.tabs.query({currentWindow: true}, tabs => {
@@ -39,25 +41,29 @@ function aria2Manager() {
 }
 
 function aria2StartUp() {
+    if (aria2Retry) {
+        clearTimeout(aria2Retry);
+    }
     if (aria2Socket && aria2Socket.readyState === 1) {
         aria2Socket.close();
     }
     aria2RPC = new Aria2(aria2Store['jsonrpc_uri'], aria2Store['jsonrpc_token']);
     aria2RPC.call('aria2.tellActive').then(result => {
         chrome.browserAction.setBadgeBackgroundColor({color: '#3cc'});
-        var active = result.map(({gid}) => gid);
-        aria2Badge(active.length);
+        aria2Retry = null;
+        aria2Active = result.map(({gid}) => gid);
+        aria2Badge(aria2Active.length);
         aria2Socket = new WebSocket(aria2Store['jsonrpc_uri'].replace('http', 'ws'));
         aria2Socket.onmessage = async event => {
             var {method, params: [{gid}]} = JSON.parse(event.data);
             if (method !== 'aria2.onBtDownloadComplete') {
                 if (method === 'aria2.onDownloadStart') {
-                    if (active.indexOf(gid) === -1) {
-                        active.push(gid);
+                    if (aria2Active.indexOf(gid) === -1) {
+                        aria2Active.push(gid);
                     }
                 }
                 else {
-                    active.splice(active.indexOf(gid), 1);
+                    aria2Active.splice(aria2Active.indexOf(gid), 1);
                     if (method === 'aria2.onDownloadComplete') {
                         var {bittorrent, files} = await aria2RPC.call('aria2.tellStatus', [gid]);
                         var name = getDownloadName(bittorrent, files);
@@ -65,11 +71,12 @@ function aria2StartUp() {
                     }
                 }
             }
-            aria2Badge(active.length);
+            aria2Badge(aria2Active.length);
         };
     }).catch(error => {
         chrome.browserAction.setBadgeBackgroundColor({color: '#c33'});
         aria2Badge('E');
+        aria2Retry = setTimeout(aria2StartUp, aria2Store['manager_interval'])
     });
 }
 
