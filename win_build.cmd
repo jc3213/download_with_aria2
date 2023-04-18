a5b77487fa5a2fcf90a674a2ea8aab1f57c154af
jc3213 <jc3213@users.noreply.github.com>
2023-04-18 10:39:05 +0800

    don't check 7-zip installation

--- win_build.cmd
+++ win_build.cmd
@@ -1,7 +1,6 @@
 @ECHO OFF
 TITLE Download with Aria2
 FOR /F "tokens=1,2*" %%I IN ('REG QUERY HKLM\Software\7-Zip /V Path') DO (IF "%%I"=="Path" SET Zip=%%K7z.exe)
-IF NOT EXIST "%Zip%" GOTO :Exit
 :Type
 ECHO ================================================================
 ECHO 1. Chromium
