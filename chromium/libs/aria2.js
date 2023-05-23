68bccb4215e123782b85719b5659c7f83cb05b67
jc3213 <jc3213@users.noreply.github.com>
2023-05-23 09:37:54 +0800

    use template literals and clean up

--- chromium/libs/aria2.js
+++ chromium/libs/aria2.js
@@ -3,10 +3,10 @@ class Aria2 {
         var [protocol, http, socket] = url.match(/(^http)s?|^(ws)s?|^([^:]+)/);
         this.post = http ? this.fetch : socket ? this.websocket : this.error(protocol);
         this.jsonrpc = url;
-        this.params = secret ? ['token:' + secret] : [];
+        this.params = secret ? [`token:${secret}`] : [];
     }
     error (protocol) {
-        throw new Error('Invalid protocol: "' + protocol + '" is not supported.');
+        throw new Error(`Invalid protocol: "${protocol}" is not supported.`);
     }
     message (method, options) {
         var params = Array.isArray(options) ? [...this.params, ...options] : [...this.params];
@@ -54,11 +54,5 @@ class Aria2 {
                 resolve(json);
             };
         });
-    }
-    get methods () {
-        return ['aria2.addUri', 'aria2.addTorrent', 'aria2.addMetalink', 'aria2.remove', 'aria2.forceRemove', 'aria2.pause', 'aria2.pauseAll', 'aria2.forcePause', 'aria2.forcePauseAll', 'aria2.unpause', 'aria2.unpauseAll', 'aria2.tellStatus', 'aria2.getUris', 'aria2.getFiles', 'aria2.getPeers', 'aria2.getServers', 'aria2.tellActive', 'aria2.tellWaiting', 'aria2.tellStopped', 'aria2.changePosition', 'aria2.changeUri', 'aria2.getOption', 'aria2.changeOption', 'aria2.getGlobalOption', 'aria2.changeGlobalOption', 'aria2.getGlobalStat', 'aria2.purgeDownloadResult', 'aria2.removeDownloadResult', 'aria2.getVersion', 'aria2.getSessionInfo', 'aria2.shutdown', 'aria2.forceShutdown', 'aria2.saveSession', 'system.multicall', 'system.listMethods', 'system.listNotifications'];
-    }
-    get notifications () {
-        return ['aria2.onDownloadStart', 'aria2.onDownloadPause', 'aria2.onDownloadStop', 'aria2.onDownloadComplete', 'aria2.onDownloadError', 'aria2.onBtDownloadComplete'];
     }
 }
