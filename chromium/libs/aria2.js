11d735e2dd3d657539b6a0eee77b766ae85d8513
jc3213 <jc3213@users.noreply.github.com>
2023-07-18 14:08:54 +0800

    reduced function call - aria2.js

--- chromium/libs/aria2.js
+++ chromium/libs/aria2.js
@@ -1,6 +1,6 @@
 class Aria2 {
     constructor (url, secret) {
-        var [protocol, http, socket] = url.match(/(^http)s?|^(ws)s?|^([^:]+)/);
+        const [protocol, http, socket] = url.match(/(^http)s?|^(ws)s?|^([^:]+)/);
         this.post = http ? this.fetch : socket ? this.websocket : this.error(protocol);
         this.jsonrpc = url;
         this.params = secret ? [`token:${secret}`] : [];
@@ -8,12 +8,8 @@ class Aria2 {
     error (protocol) {
         throw new Error(`Invalid protocol: "${protocol}" is not supported.`);
     }
-    message (method, options) {
-        var params = [...this.params, ...options];
-        return {id: '', jsonrpc: '2.0', method, params};
-    }
     call (method, ...options) {
-        var json = this.message(method, options);
+        const json = {id: '', jsonrpc: '2.0', method, params: [...this.params, ...options]};
         return this.post(JSON.stringify(json)).then(({result, error}) => {
             if (result) {
                 return result;
@@ -22,7 +18,7 @@ class Aria2 {
         });
     }
     batch (array) {
-        var json = array.map(([method, ...options]) => this.message(method, options));
+        const json = array.map(([method, ...options]) => ({id: '', jsonrpc: '2.0', method, params: [...this.params, ...options]}));
         return this.post(JSON.stringify(json)).then((response) => {
             return response.map(({result, error}) => {
                 if (result) {
@@ -42,7 +38,7 @@ class Aria2 {
     }
     websocket (message) {
         return new Promise((resolve, reject) => {
-            var socket = new WebSocket(this.jsonrpc);
+            const socket = new WebSocket(this.jsonrpc);
             socket.onopen = (event) => socket.send(message);
             socket.onclose = reject;
             socket.onmessage = (event) => {
