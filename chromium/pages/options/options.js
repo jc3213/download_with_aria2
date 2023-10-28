0fb4eed1621816afc4f44ac4ddc72e7d1d3dd19c
jc3213 <jc3213@users.noreply.github.com>
2023-10-28 17:12:42 +0800

    updated i18n, fixed logic

--- chromium/pages/options/options.js
+++ chromium/pages/options/options.js
@@ -38,7 +38,7 @@ var mapped = document.querySelectorAll('[data-map]');
 var listed = {};
 var listLET = document.querySelector('.template > .map');
 
-if (typeof browser !== 'undefined') {
+if (typeof browser === 'function') {
     extension.classList.add('firefox');
     var firefox = document.createElement('script');
     firefox.src = 'options_firefox.js';
