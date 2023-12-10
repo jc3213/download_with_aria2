f311b79e894832b04a5bfd5e81ac194f40e0b8e8
jc3213 <jc3213@users.noreply.github.com>
2023-12-10 16:09:05 +0800

    v4.6.0.2275

--- win_build.cmd
+++ win_build.cmd
@@ -9,22 +9,20 @@ echo 2. Chromium Manifest V3
 echo 3. Firefox
 echo ================================================================
 set /p act=^> 
-if [%act%] equ [1] set bow=chromium
-if [%act%] equ [2] set bow=chromium_mv3
-if [%act%] equ [3] set bow=firefox
-if not defined bow goto :main
-for /f "usebackq skip=3 tokens=1,2 delims=,: " %%a in (%bow%\manifest.json) do (if %%~a equ version set output=%bow%-%%~b.zip)
-if %bow% equ chromium_mv3 set ext=-x!"expansion.js"
-"%zip%" a "%output%" "%~dp0chromium\*" %ext%
-if %bow% equ chromium goto :exit
-"%zip%" u "%output%" -ux2 "%~dp0%bow%\*"
+if [%act%] equ [1] call :archive "chromium" "Chromium"
+if [%act%] equ [2] call :archive "chromium_mv3" "Chromium Manifest V3" "-x!"expansion.js""
+if [%act%] equ [3] call :archive "firefox" "Firefox"
+goto :main
+:archive
+for /f "usebackq skip=3 tokens=1,2 delims=,: " %%a in (%~1\manifest.json) do (if %%~a equ version set output=%~1-%%~b.zip)
+"%zip%" a "%output%" "%~dp0chromium\*" %~3 >nul
+if %~1 equ chromium goto :exit
+"%zip%" u "%output%" -ux2 "%~dp0%~1\*" >nul
 :exit
 echo.
+echo The program has built the extension for "%~2"
 echo.
-echo File "%output%"
-echo Return to main menu in 5 seconds...
+echo The output file: "%output%"
 timeout /t 5
 set act=
-set bow=
-set ext=
 goto :main
