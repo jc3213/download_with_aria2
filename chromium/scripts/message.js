3e616796e37ee918a180817119dda24afd74bca8
jc3213 <jc3213@users.noreply.github.com>
2024-01-23 09:22:30 +0800

    Update message.js

--- chromium/scripts/message.js
+++ chromium/scripts/message.js
@@ -9,9 +9,9 @@ window.addEventListener('load', (event) => {
 });
 
 window.addEventListener('message', (event) => {
-    var {aria2c, download} = event.data;
-    if (aria2c === 'Download With Aria2' && download !== undefined) {
-        chrome.runtime.sendMessage({action: 'external_download', params: download});
+    var {aria2c, params} = event.data;
+    if (aria2c === 'aria2c-jsonrpc-call' || params !== undefined) {
+        chrome.runtime.sendMessage({action: 'external_download', params});
     }
 });
 
