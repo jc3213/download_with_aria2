72dbe3474e7cd431d12989c6cbf06cf45312718c
jc3213 <jc3213@users.noreply.github.com>
2024-01-29 19:25:13 +0800

    Update message.js

--- chromium/scripts/message.js
+++ chromium/scripts/message.js
@@ -10,7 +10,7 @@ window.addEventListener('load', (event) => {
 
 window.addEventListener('message', (event) => {
     var {aria2c, params} = event.data;
-    if (aria2c === 'aria2c-jsonrpc-call' || params !== undefined) {
+    if (aria2c === 'aria2c-jsonrpc-call') {
         chrome.runtime.sendMessage({action: 'external_download', params});
     }
 });
