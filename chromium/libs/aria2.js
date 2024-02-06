5782f5fdbb25f3aa9f434bfbc9d8cd1a923a3072
jc3213 <jc3213@users.noreply.github.com>
2024-02-06 14:41:46 +0800

    Update aria2.js

--- chromium/libs/aria2.js
+++ chromium/libs/aria2.js
@@ -1,41 +1,47 @@
 class Aria2 {
     constructor (url, secret) {
         const [protocol, http, socket] = url.match(/(https?)|(wss?)|([^:]+)/);
-        this.post = http ? this.fetch : socket ? this.websocket : this.handle({error: new Error(`Invalid protocol: "${protocol}" is not supported.`)});
+        this.post = http ? this.fetch : socket ? this.send : this.handler({error: new Error(`Invalid protocol: "${protocol}" is not supported.`)});
         this.jsonrpc = url;
         this.secret = `token:${secret}`;
+        this.websocket = this.connect(url.replace('http', 'ws'));
     }
-    call (method, ...options) {
-        const json = {id: '', jsonrpc: '2.0', method, params: [this.secret, ...options]};
-        return this.post(JSON.stringify(json)).then(this.handle);
+    connect (url) {
+        return new Promise((resolve, reject) => {
+            const websocket = new WebSocket(url);
+            websocket.onopen = (event) => resolve(websocket);
+            websocket.onerror = (error) => reject(error);
+        });
     }
-    batch (array) {
-        const json = array.map(([method, ...options]) => ({id: '', jsonrpc: '2.0', method, params: [this.secret, ...options]}));
-        return this.post(JSON.stringify(json)).then((response) => response.map(this.handle));
-    }
-    handle ({result, error}) {
+    handler ({result, error}) {
         if (result) {
             return result;
         }
         throw error;
     }
+    message ([method, ...options]) {
+        return {id: '', jsonrpc: '2.0', method, params: [this.secret, ...options]};
+    }
+    call (...message) {
+        const json = this.message(message);
+        return this.post(JSON.stringify(json)).then(this.handler);
+    }
+    batch (messages) {
+        const json = messages.map(message => this.message(message));
+        return this.post(JSON.stringify(json)).then((response) => response.map(this.handler));
+    }
     fetch (body) {
         return fetch(this.jsonrpc, {method: 'POST', body}).then((response) => {
-            if (response.ok) {
-                return response.json();
-            }
+            if (response.ok) { return response.json(); }
             throw new Error(response.statusText);
         });
     }
-    websocket (message) {
-        return new Promise((resolve, reject) => {
-            const socket = new WebSocket(this.jsonrpc);
-            socket.onopen = (event) => socket.send(message);
-            socket.onclose = reject;
-            socket.onmessage = (event) => {
-                socket.close();
-                resolve(JSON.parse(event.data));
-            };
+    send (message) {
+        return new Promise(async (resolve, reject) => {
+            const websocket = await this.websocket;
+            websocket.onmessage = (event) => resolve(JSON.parse(event.data));
+            websocket.onerror = (error) => reject(error);
+            websocket.send(message);
         });
     }
 }
