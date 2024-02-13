3320279dfdcb3dab86f6fe563c8125917190b0f4
jc3213 <jc3213@users.noreply.github.com>
2024-02-13 10:49:57 +0800

    Update win_build.cmd

--- win_build.cmd
+++ win_build.cmd
@@ -10,12 +10,12 @@ echo 3. Firefox
 echo ================================================================
 set /p act=^> 
 if [%act%] equ [1] call :archive "chromium" "Chromium"
-if [%act%] equ [2] call :archive "chromium_mv3" "Chromium Manifest V3" "-x!"expansion.js""
+if [%act%] equ [2] call :archive "chromium_mv3" "Chromium Manifest V3"
 if [%act%] equ [3] call :archive "firefox" "Firefox"
 goto :main
 :archive
 for /f "usebackq skip=3 tokens=1,2 delims=,: " %%a in (%~1\manifest.json) do (if %%~a equ version set output=%~1-%%~b.zip)
-"%zip%" a "%output%" "%~dp0chromium\*" %~3 >nul
+"%zip%" a "%output%" "%~dp0chromium\*" >nul
 if %~1 equ chromium goto :exit
 "%zip%" u "%output%" -ux2 "%~dp0%~1\*" >nul
 :exit
