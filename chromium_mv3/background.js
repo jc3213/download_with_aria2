4fc06e36d315b1afa1422104dcb96bf8ad178628
jc3213 <jc3213@users.noreply.github.com>
2024-02-14 11:10:21 +0800

    better capture logic

--- chromium_mv3/background.js
+++ chromium_mv3/background.js
@@ -1,17 +1,47 @@
 importScripts('libs/aria2.js', 'libs/core.js', 'libs/tools.js', 'crossbrowser.js');
 
+var aria2History = {};
+var aria2Monitor = {};
+var aria2Persistent = setInterval(chrome.runtime.getPlatformInfo, 25e3);
+
 chrome.downloads.onDeterminingFilename.addListener(async ({id, finalUrl, referrer, filename, fileSize}) => {
-    if (!aria2Storage['capture_enabled'] || testUrlScheme(finalUrl)) {
+    if (!aria2Storage['capture_enabled') {
         return;
+    }
+    if (aria2History[finalUrl]) {
+        delete aria2History[finalUrl];
+        return;
+    }
+    chrome.downloads.erase({id});
+    aria2Monitor[id] = {url: finalUrl, referrer, filename, fileSize};
+    aria2History[finalUrl] = id;
+
+});
+
+chrome.downloads.onErased.addListener(async (id) => {
+    if (!aria2Monitor[id]) {
+        return;
+    }
+    var {url, referrer, filename, fileSize} = aria2Monitor[id];
+    if (testUrlScheme(url)) {
+        return aria2NativeDownload(url, referrer, filename);
     }
     var referer = referrer === '' ? await getCurrentTabUrl() : referrer;
     var hostname = getHostname(referer);
     var captured = aria2CaptureResult(hostname, getFileExtension(filename), fileSize);
     if (captured) {
-        chrome.downloads.erase({id});
-        aria2Download(finalUrl, {out: filename}, referer, hostname);
+        delete aria2History[url];
+        return aria2Download(url, {out: filename}, referer, hostname);
     }
+    aria2NativeDownload(url, referrer, filename);
 });
+
+function aria2NativeDownload(url, referrer, filename) {
+    chrome.cookies.getAll({url}, (cookies) => {
+        var headers = cookies.map(({name, value}) => ({name, value}));
+        chrome.downloads.download({url, filename, headers: [{name: 'Referrer', value: referrer}, ...headers]});
+    });
+}
 
 chrome.storage.sync.get(null).then((json) => {
     aria2Storage = {...aria2Default, ...json};
@@ -20,5 +50,3 @@ chrome.storage.sync.get(null).then((json) => {
     aria2TaskManager();
     aria2ContextMenus();
 });
-
-aria2Persistent = setInterval(chrome.runtime.getPlatformInfo, 25e3);
