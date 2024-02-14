a56a13e9e2dca8fd6514ec630f1b136821d91597
jc3213 <jc3213@users.noreply.github.com>
2024-02-14 11:05:32 +0800

    better capture logic

--- chromium/background.js
+++ chromium/background.js
@@ -1,21 +1,45 @@
-async function captureOnFilename({id, finalUrl, referrer, filename, fileSize}) {
-    if (testUrlScheme(finalUrl)) {
+var aria2History = {};
+var aria2Monitor = {};
+
+function captureOnFilename({id, finalUrl, referrer, filename, fileSize}) {
+    if (aria2History[finalUrl]) {
+        delete aria2History[finalUrl];
         return;
+    }
+    chrome.downloads.erase({id});
+    aria2Monitor[id] = {url: finalUrl, referrer, filename, fileSize};
+    aria2History[finalUrl] = id;
+}
+
+async function captureOnErased(id) {
+    var {url, referrer, filename, fileSize} = aria2Monitor[id];
+    if (testUrlScheme(url)) {
+        return aria2NativeDownload(url, referrer, filename);
     }
     var referer = referrer === '' ? await getCurrentTabUrl() : referrer;
     var hostname = getHostname(referer);
     var captured = aria2CaptureResult(hostname, getFileExtension(filename), fileSize);
     if (captured) {
-        chrome.downloads.erase({id});
-        aria2Download(finalUrl, {out: filename}, referer, hostname);
+        delete aria2History[url];
+        return aria2Download(url, {out: filename}, referer, hostname);
     }
+    aria2NativeDownload(url, referrer, filename);
+}
+
+function aria2NativeDownload(url, referrer, filename) {
+    chrome.cookies.getAll({url}, (cookies) => {
+        var headers = cookies.map(({name, value}) => ({name, value}));
+        chrome.downloads.download({url, filename, headers: [{name: 'Referrer', value: referrer}, ...headers]});
+    });
 }
 
 function aria2CaptureSwitch() {
     if (aria2Storage['capture_enabled']) {
-        return chrome.downloads.onDeterminingFilename.addListener(captureOnFilename);
+        chrome.downloads.onDeterminingFilename.addListener(captureOnFilename);
+        return chrome.downloads.onErased.addListener(captureOnErased);
     }
     chrome.downloads.onDeterminingFilename.removeListener(captureOnFilename);
+    chrome.downloads.onErased.removeListener(captureOnErased);
 }
 
 chrome.action = chrome.browserAction;
