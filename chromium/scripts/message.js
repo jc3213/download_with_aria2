a15bb3451c12aacf5dbe64a18da6f4c6818da574
jc3213 <jc3213@users.noreply.github.com>
2024-02-20 13:35:03 +0800

    Update message.js

--- chromium/scripts/message.js
+++ chromium/scripts/message.js
@@ -4,29 +4,33 @@ var header = ['Cookie: ' + document.cookie];
 window.addEventListener('message', (event) => {
     switch(event.data.aria2c) {
         case 'aria2c-jsonrpc-echo':
-            window.postMessage({extension_name: 'Download With Aria2', source: 'aria2c'});
+            aria2RPCEcho();
             break;
         case 'aria2c-jsonrpc-call':
-            aria2SendRPCCall(event.data.params);
+            aria2RPCCall(event.data.params);
             break;
     }
 });
 
-chrome.runtime.onMessage.addListener(({query}, sender, response) => {
-    switch (query) {
-        case 'aria2c_all_images':
-            response({images: getAllImages(), options: {referer, header}});
-            break;
-    }
-});
+function aria2RPCEcho() {
+    window.postMessage({extension_name: 'Download With Aria2', source: 'aria2c'});
+}
 
-function aria2SendRPCCall(params) {
+function aria2RPCCall(params) {
     if (params?.urls || params?.files?.torrents || params?.files?.metalinks) {
         chrome.runtime.sendMessage({action: 'message_download', params});
     }
 }
 
-function getAllImages(archive = {}, result = []) {
+chrome.runtime.onMessage.addListener(({query}, sender, response) => {
+    switch (query) {
+        case 'aria2c_all_images':
+            response({images: queryAllImages(), options: {referer, header}});
+            break;
+    }
+});
+
+function queryAllImages(archive = {}, result = []) {
     [...document.images].forEach((img) => {
         var {src, alt} = img;
         if (src === referer || src.startsWith('data') || src in archive) {
