44763addaa4ee6d68575ffdab4a7fbaf8f90116a
jc3213 <jc3213@users.noreply.github.com>
2024-03-18 08:47:51 +0800

    optimized performance

--- chromium_mv3/background.js
+++ chromium_mv3/background.js
@@ -4,7 +4,7 @@ chrome.downloads.onDeterminingFilename.addListener(({id, finalUrl, referrer, fil
     if (!aria2Storage['capture_enabled'] || finalUrl.startsWith('data') || finalUrl.startsWith('blob')) {
         return;
     }
-    var hostname = referrer === '' ? getHostname(finalUrl) : getHostname(referrer);
+    var hostname = referrer ? getHostname(referrer) : getHostname(finalUrl);
     var captured = aria2CaptureResult(hostname, getFileExtension(filename), fileSize);
     if (captured) {
         chrome.downloads.erase({id});
