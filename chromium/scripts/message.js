048bea42e4790a171ecae129cecd487fba30054a
jc3213 <jc3213@users.noreply.github.com>
2024-10-26 22:36:03 +0800

    oops

--- chromium/scripts/message.js
+++ chromium/scripts/message.js
@@ -1,7 +1,7 @@
 window.addEventListener('message', (event) => {
     switch(event.data.aria2c) {
         case 'aria2c_jsonrpc_echo':
-            aria2RPCEcho(event.data.alert);
+            aria2RPCEcho();
             break;
         case 'aria2c_jsonrpc_call':
             aria2RPCCall(event.data.params);
@@ -9,7 +9,7 @@ window.addEventListener('message', (event) => {
     }
 });
 
-function aria2RPCEcho(bool) {
+function aria2RPCEcho() {
     var {name, version} = chrome.runtime.getManifest();
     var message = chrome.i18n.getMessage('extension_echo').replace('{name}', name).replace('{version}', version);
     window.postMessage({ extension_name: name, extension_version: version, message});
