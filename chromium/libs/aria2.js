d4029044950a87795dc5a878574caa307f819134
jc3213 <jc3213@users.noreply.github.com>
2025-04-19 15:55:41 +0800

    Update aria2.js to v0.9

--- chromium/libs/aria2.js
+++ chromium/libs/aria2.js
@@ -6,14 +6,13 @@ class Aria2 {
         this.url = path[2];
         this.secret = path[3];
     }
-    version = '0.8.1';
+    version = '0.9';
     args = { retries: 10, timeout: 10000 };
     set scheme (scheme) {
         let method = scheme.match(/^(http|ws)s?$/)?.[1];
         if (!method) { throw new Error('Unsupported JSON-RPC scheme: "' + scheme + '"'); }
-        this.call = this[scheme];
         this.args.scheme = scheme;
-        this.args.path = scheme + '://' + this.args.url;
+        this.call = method === 'ws' ? this.send : this.post;
     }
     get scheme () {
         return this.args.scheme;
@@ -23,7 +22,7 @@ class Aria2 {
         this.args.url = url;
         this.args.path = this.args.scheme + '://' + url;
         this.args.ws = this.args.path.replace('http', 'ws');
-        this.args.count = 0;
+        this.args.tries = 0;
         this.disconnect();
         this.connect();
     }
@@ -43,11 +42,10 @@ class Aria2 {
         return isNaN(this.args.retries) ? Infinity : this.args.retries;
     }
     set timeout (number) {
-        this.args.time = isNaN(number) ? 10 : number | 0;
-        this.args.timeout = this.args.time * 1000;
+        this.args.timeout = isNaN(number) ? 10000 : number * 1000;
     }
     get timeout () {
-        return isNaN(this.args.time) ? 10 : this.args.time | 0;
+        return isNaN(this.args.timeout) ? 10 : this.args.timeout / 1000;
     }
     set onopen (callback) {
         this.args.onopen = typeof callback === 'function' ? callback : null;
@@ -75,30 +73,29 @@ class Aria2 {
         };
         this.socket.onmessage = (event) => {
             let response = JSON.parse(event.data);
-            if (!response.method) { this.socket.resolve(response); }
+            if (!response.method) { this.args.onresponse(response); }
             else if (typeof this.args.onmessage === 'function') { this.args.onmessage(response); }
         };
         this.socket.onclose = (event) => {
             this.alive = false;
-            if (!event.wasClean && this.args.count < this.args.retries) { setTimeout(() => this.connect(), this.args.timeout); }
+            if (!event.wasClean && this.args.tries ++ < this.args.retries) { setTimeout(() => this.connect(), this.args.timeout); }
             if (typeof this.args.onclose === 'function') { this.args.onclose(event); }
-            this.args.count ++;
         };
     }
     disconnect () {
         this.socket?.close();
     }
-    http (...args) {
+    send (...args) {
+        return new Promise((resolve, reject) => {
+            this.args.onresponse = resolve;
+            this.socket.onerror = reject;
+            this.socket.send(this.json(args));
+        });
+    }
+    post (...args) {
         return fetch(this.args.path, {method: 'POST', body: this.json(args)}).then((response) => {
             if (response.ok) { return response.json(); }
             throw new Error(response.statusText);
-        });
-    }
-    ws (...args) {
-        return new Promise((resolve, reject) => {
-            this.socket.resolve = resolve;
-            this.socket.onerror = reject;
-            this.socket.send(this.json(args));
         });
     }
     json (args) {
