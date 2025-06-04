db4afa0daa04a91a0493985b1655b169bde99638
jc3213 <jc3213@users.noreply.github.com>
2025-06-04 13:42:34 +0800

    use BroadcastChannel to avoid spams from window.postMessage

--- chromium/scripts/message.js
+++ chromium/scripts/message.js
@@ -1,14 +1,16 @@
+let channel = new BroadcastChannel('DownloadWithAria2');
+
 function aria2EchoResponse() {
     let {name, version} = chrome.runtime.getManifest();
-    window.postMessage({ aria2c: 'aria2c_response_echo', name, version });
+    channel.postMessage({ aria2c: 'aria2c_response_echo', name, version });
 }
 
-function aria2CallHandler (args) {
+function aria2CallHandler(args) {
     let params = ( Array.isArray(args) ? args : [args] ).filter( (entry) => entry?.url );
     chrome.runtime.sendMessage({ action: 'jsonrpc_download', params });
 }
 
-window.addEventListener('message', (event) => {
+channel.onmessage = (event) => {
     let {aria2c, params} = event.data;
     switch (aria2c) {
         case 'aria2c_jsonrpc_echo':
