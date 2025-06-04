24ba01420573b6b33a5d80b83751bb9295523744
jc3213 <jc3213@users.noreply.github.com>
2025-06-04 13:44:06 +0800

    v4.16.2.3422

--- chromium/scripts/message.js
+++ chromium/scripts/message.js
@@ -1,15 +1,4 @@
 let channel = new BroadcastChannel('DownloadWithAria2');
-
-function aria2EchoResponse() {
-    let {name, version} = chrome.runtime.getManifest();
-    channel.postMessage({ aria2c: 'aria2c_response_echo', name, version });
-}
-
-function aria2CallHandler(args) {
-    let params = ( Array.isArray(args) ? args : [args] ).filter( (entry) => entry?.url );
-    chrome.runtime.sendMessage({ action: 'jsonrpc_download', params });
-}
-
 channel.onmessage = (event) => {
     let {aria2c, params} = event.data;
     switch (aria2c) {
@@ -21,3 +10,13 @@ channel.onmessage = (event) => {
             break;
     };
 });
+
+function aria2EchoResponse() {
+    let {name, version} = chrome.runtime.getManifest();
+    channel.postMessage({ aria2c: 'aria2c_response_echo', name, version });
+}
+
+function aria2CallHandler(args) {
+    let params = ( Array.isArray(args) ? args : [args] ).filter( (entry) => entry?.url );
+    chrome.runtime.sendMessage({ action: 'jsonrpc_download', params });
+}
