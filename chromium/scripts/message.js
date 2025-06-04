bb990a6ed502ab505d41f715f287aef45b613a31
jc3213 <jc3213@users.noreply.github.com>
2025-06-04 14:21:07 +0800

    reverted BroadcastChannel though

--- chromium/scripts/message.js
+++ chromium/scripts/message.js
@@ -1,5 +1,14 @@
-let channel = new BroadcastChannel('DownloadWithAria2');
-channel.onmessage = (event) => {
+function aria2EchoResponse() {
+    let {name, version} = chrome.runtime.getManifest();
+    window.postMessage({ aria2c: 'aria2c_response_echo', name, version });
+}
+
+function aria2CallHandler (args) {
+    let params = ( Array.isArray(args) ? args : [args] ).filter( (entry) => entry?.url );
+    chrome.runtime.sendMessage({ action: 'jsonrpc_download', params });
+}
+
+window.addEventListener('message', (event) => {
     let {aria2c, params} = event.data;
     switch (aria2c) {
         case 'aria2c_jsonrpc_echo':
@@ -10,13 +19,3 @@ channel.onmessage = (event) => {
             break;
     };
 });
-
-function aria2EchoResponse() {
-    let {name, version} = chrome.runtime.getManifest();
-    channel.postMessage({ aria2c: 'aria2c_response_echo', name, version });
-}
-
-function aria2CallHandler(args) {
-    let params = ( Array.isArray(args) ? args : [args] ).filter( (entry) => entry?.url );
-    chrome.runtime.sendMessage({ action: 'jsonrpc_download', params });
-}
