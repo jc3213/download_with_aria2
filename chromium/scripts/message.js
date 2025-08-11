710a222e1049e3089508eaaba4fd31c0110b6455
jc3213 <jc3213@users.noreply.github.com>
2025-08-11 21:05:09 +0800

    object-dispatch for message

--- chromium/scripts/message.js
+++ chromium/scripts/message.js
@@ -1,24 +1,16 @@
-function aria2EchoResponse() {
-    let {name, version} = chrome.runtime.getManifest();
-    window.postMessage({ aria2c: 'aria2c_response_echo', name, version });
-}
-
-function aria2CallHandler (args) {
-    let params = ( Array.isArray(args) ? args : [args] ).filter( (entry) => entry?.url );
-    chrome.runtime.sendMessage({ action: 'jsonrpc_download', params });
-}
+const messageHandlers = {
+    'aria2c_jsonrpc_echo': () => {
+        let {name, version} = chrome.runtime.getManifest();
+        window.postMessage({ aria2c: 'aria2c_response_echo', name, version });
+    },
+    'aria2c_jsonrpc_call': (arg) => {
+        let params = ( Array.isArray(arg) ? arg : [arg] ).filter( (entry) => entry?.url );
+        chrome.runtime.sendMessage({ action: 'jsonrpc_download', params });
+    }
+};
 
 window.addEventListener('message', (event) => {
     let {aria2c, params} = event.data;
-    if (!aria2c) {
-        return;
-    }
-    switch (aria2c) {
-        case 'aria2c_jsonrpc_echo':
-            aria2EchoResponse();
-            break;
-        case 'aria2c_jsonrpc_call':
-            aria2CallHandler(params);
-            break;
-    };
+    let handler = messageHandlers[aria2c];
+    handler?.(params);
 });
