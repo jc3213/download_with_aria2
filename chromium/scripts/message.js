a989b866756e763b858a1edff481d75490ddf9ab
jc3213 <jc3213@users.noreply.github.com>
2025-08-13 22:44:06 +0800

    message port handling download with result

--- chromium/scripts/message.js
+++ chromium/scripts/message.js
@@ -4,8 +4,11 @@ const messageHandlers = {
         window.postMessage({ aria2c: 'aria2c_response', id, result });
     },
     'aria2c_download': (id, args) => {
-        let params = args.map(({ url, options }) => ({ name: url, task: { method: 'aria2.addUri', params: [[url], options] } }));
-        chrome.runtime.sendMessage({ action: 'jsonrpc_download', params });
+        let params = args.filter((arg) => arg?.url)
+            .map(({ url, options = {} }) => ({ name: url, task: { method: 'aria2.addUri', params: [[url], options] } }));
+        chrome.runtime.sendMessage({ action: 'jsonrpc_download', params }, (result) => {
+            window.postMessage({ aria2c: 'aria2c_response', id, result });
+        });
     }
 };
 
