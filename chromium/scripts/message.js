bba6ab7fcd1dc0fc29d910e2bfd702b5f4a9a4b3
jc3213 <jc3213@users.noreply.github.com>
2025-08-15 12:20:51 +0800

    clean up

--- chromium/scripts/message.js
+++ chromium/scripts/message.js
@@ -1,15 +1,15 @@
+function messgeChannel(id, message) {
+    chrome.runtime.sendMessage(message, (result) => {
+        window.postMessage({ aria2c: 'aria2c_response', id, result });
+    });
+}
+
 const messageHandlers = {
-    'aria2c_status': (id) => {
-        chrome.runtime.sendMessage({ action: 'system_runtime' }, (result) => {
-            window.postMessage({ aria2c: 'aria2c_response', id, result });
-        });
-    },
+    'aria2c_status': (id) => messgeChannel(id, { action: 'system_runtime' }),
     'aria2c_download': (id, args) => {
         let params = args.filter((arg) => arg?.url)
             .map(({ url, options = {} }) => ({ name: url, task: { method: 'aria2.addUri', params: [[url], options] } }));
-        chrome.runtime.sendMessage({ action: 'jsonrpc_download', params }, (result) => {
-            window.postMessage({ aria2c: 'aria2c_response', id, result });
-        });
+        messgeChannel(id, { action: 'jsonrpc_download', params });
     }
 };
 
