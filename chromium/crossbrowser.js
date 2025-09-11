db36a6f0fab97ba33638f48403660e10d2f294c1
jc3213 <jc3213@users.noreply.github.com>
2025-09-11 12:01:35 +0800

    Update crossbrowser.js

--- chromium/crossbrowser.js
+++ chromium/crossbrowser.js
@@ -271,9 +271,8 @@ async function whenNotification(gid, type) {
     let [{ result }] = await aria2RPC.call({ method: 'aria2.tellStatus', params: [gid] });
     let { bittorrent, files } = result;
     let [{ path, uris }] = files;
-    let [{ uri }] = uris;
     let title = chrome.i18n.getMessage('download_' + type);
-    let message = bittorrent?.info?.name ?? path?.slice(path.lastIndexOf('/') + 1) ?? uri;
+    let message = bittorrent?.info?.name ?? path?.slice(path.lastIndexOf('/') + 1) ?? uris?.[0]?.uri ?? gid;
     showNotification(title, message);
 }
 
