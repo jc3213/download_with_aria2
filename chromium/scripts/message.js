140918b705a3fc0b5664c51f55d691808470a8d3
jc3213 <jc3213@users.noreply.github.com>
2025-12-08 20:27:16 +0800

    v4.22.1.3920

--- chromium/scripts/message.js
+++ chromium/scripts/message.js
@@ -4,16 +4,23 @@ function messgeChannel(id, message) {
     });
 }
 
-const messageHandlers = {
-    'aria2c_status': (id) => messgeChannel(id, { action: 'system_runtime' }),
-    'aria2c_download': (id, args) => {
-        let params = args.filter((arg) => arg?.url)
-            .map(({ url, options = {} }) => ({ method: 'aria2.addUri', params: [[url], options] }));
-        messgeChannel(id, { action: 'jsonrpc_download', params });
+function downloadHandler(id, args) {
+    let params = [];
+    for (let arg of args) {
+        if (arg?.url) {
+            let { url, options = {} } = arg;
+            params.push({ method: 'aria2.addUri', params: [[url], options] });
+        }
     }
+    messgeChannel(id, { action: 'jsonrpc_download', params });
+}
+
+const messageDispatch = {
+    'aria2c_status': (id) => messgeChannel(id, { action: 'system_runtime' }),
+    'aria2c_download': downloadHandler
 };
 
 window.addEventListener('message', (event) => {
     let { aria2c, id, params } = event.data;
-    messageHandlers[aria2c]?.(id, params);
+    messageDispatch[aria2c]?.(id, params);
 });
