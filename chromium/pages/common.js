69c8606c729d5af6764c2ae6c34e725b7fc8b8c9
jc3213 <jc3213@users.noreply.github.com>
2026-03-27 21:38:49 +0800

    support multiple hotkeys, github like ui

--- chromium/pages/common.js
+++ chromium/pages/common.js
@@ -1,10 +1,5 @@
 const hotkeys = {};
 
-for (let el of document.querySelectorAll('[hotkey]')) {
-    let combo = el.getAttribute('hotkey').toLowerCase();
-    hotkeys[combo] = el;
-}
-
 for (let el of document.querySelectorAll('[i18n]')) {
     let i18n = el.getAttribute('i18n');
     el.textContent = chrome.i18n.getMessage(i18n);
@@ -13,6 +8,26 @@ for (let el of document.querySelectorAll('[i18n]')) {
 for (let el of document.querySelectorAll('[i18n-tips]')) {
     let tips = el.getAttribute('i18n-tips')
     el.title = chrome.i18n.getMessage(tips);
+}
+
+for (let el of document.querySelectorAll('[hotkey]')) {
+    let keys = el.getAttribute('hotkey').toLowerCase();
+    while (true) {
+        let i = keys.indexOf(';');
+        if (i === -1) {
+            let k = keys.trim();
+            if (k) {
+                hotkeys[k] = el;
+            }
+            break;
+        } else {
+            let k = keys.substring(0, i).trim();
+            if (k) {
+                hotkeys[k] = el;
+            }
+        }
+        keys = keys.substring(i + 1);
+    }
 }
 
 document.addEventListener('keydown', (event) => {
