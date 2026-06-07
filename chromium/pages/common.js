024c0d0cf966ddd07c41bee7391c0738b674f42f
jc3213 <jc3213@users.noreply.github.com>
2026-06-07 12:03:08 +0800

    improved performance

--- chromium/pages/common.js
+++ chromium/pages/common.js
@@ -1,20 +1,28 @@
-const hotkeys = {};
+const i18nText = document.querySelectorAll('[i18n]');
+const i18nTips = document.querySelectorAll('[i18n-tips]');
 
-for (let el of document.querySelectorAll('[i18n]')) {
+for (let i = 0, l = i18nText.length; i < l; i++) {
+    let el = i18nText[i];
     let i18n = el.getAttribute('i18n');
     el.textContent = chrome.i18n.getMessage(i18n);
 }
 
-for (let el of document.querySelectorAll('[i18n-tips]')) {
+for (let i = 0, l = i18nTips.length; i < l; i++) {
+    let el = i18nTips[i];
     let tips = el.getAttribute('i18n-tips')
     el.title = chrome.i18n.getMessage(tips);
 }
 
-for (let el of document.querySelectorAll('[hotkey]')) {
-    for (let keys of el.getAttribute('hotkey').toLowerCase().split('\n')) {
-        let combo = keys.trim();
-        if (combo) {
-            hotkeys[combo] = el;
+const hotkeyMap = document.querySelectorAll('[hotkey]')
+const hotkeyCombo = {};
+
+for (let i = 0, l = hotkeyMap.length; i < l; i++) {
+    let el = hotkeyMap[i];
+    let keys = el.getAttribute('hotkey').toLowerCase().split('\n');
+    for (let j = 0, m = keys.length; j < m; j++) {
+        let k = keys[j].trim();
+        if (k) {
+            hotkeyCombo[k] = el;
         }
     }
 }
@@ -33,7 +41,7 @@ document.addEventListener('keydown', (event) => {
     }
     keys.push(key.toLowerCase());
     let combo = keys.join('+');
-    let hotkey = hotkeys[combo];
+    let hotkey = hotkeyCombo[combo];
     if (hotkey) {
         event.preventDefault();
         hotkey.click();
