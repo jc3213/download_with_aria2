3c57b6d91e99bb2a8a11521c64fbb028d3d7f564
jc3213 <jc3213@users.noreply.github.com>
2026-06-18 14:35:02 +0800

    update

--- chromium/platform.js
+++ chromium/platform.js
@@ -2,18 +2,24 @@ chrome.action = chrome.browserAction;
 
 function captureDownloads(downloadItem) {
     let url = downloadItem.finalUrl;
+
     if (url.startsWith('data') || url.startsWith('blob')) {
         return;
     }
+
     let referer = downloadItem.referrer;
     let hostname = getHostname(referer || url);
+
     if (matchHostname(captureHosts, hostname)) {
         return;
     }
+
     let id = downloadItem.id;
+
     chrome.downloads.search({ id }, () => {
         chrome.downloads.erase({ id });
     });
+
     downloadHandler(url, referer, downloadItem.filename, hostname);
 }
 
