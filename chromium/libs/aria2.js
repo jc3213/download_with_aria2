d39b0a98bad4b3397116616e783ddeb719681aa0
jc3213 <jc3213@users.noreply.github.com>
2026-09-18 22:54:57 +0800

    Update aria2.js

--- chromium/libs/aria2.js
+++ chromium/libs/aria2.js
@@ -228,7 +228,7 @@ class Aria2 {
                 session.reject(new Error('WebSocket connection closed'));
             }
 
-            this.#pending.clear();
+            pending.clear();
 
             let onclose = this.#onclose;
 
